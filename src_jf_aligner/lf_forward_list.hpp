444ae2b670def1e315780a73801d609ae35f4dd9
Guillaume Marcais <Guillaume Marcais guillaume@marcais.net>
2014-02-11 16:17:50 -0500

    Added a lock free forward list (only push_front method and iterators).

---
+++ src_jf_aligner/lf_forward_list.hpp
@@ -0,0 +1,113 @@
+#ifndef _LF_FORWARD_LIST_HPP_
+#define _LF_FORWARD_LIST_HPP_
+
+#include <memory>
+
+template<typename T, class Alloc = std::allocator<T>>
+class lf_forward_list {
+  struct head_node {
+    head_node* next_;
+  };
+  struct node : head_node {
+    T     val_;
+  };
+
+public:
+  typedef Alloc allocator_type;
+  typedef T     value_type;
+  class iterator;
+  class const_iterator;
+
+  /** Construct an empty list */
+  explicit lf_forward_list (const allocator_type& alloc = allocator_type()) :
+    T_alloc_(alloc), node_alloc_(alloc), head_(0)
+  { }
+
+  /** Push an element to the front of the list (copy) */
+  void push_front(const value_type& val) {
+    node* n = node_alloc_.allocate(1);
+    T_alloc_.construct(&n->val_, val);
+    push_front_(n);
+  }
+
+  /** Push an element to the front of the list (move) */
+  void push_front(value_type&& val) {
+    node* n = node_alloc_.allocate(1);
+    T_alloc_.construct(&n->val_, std::move(val));
+    push_front_(n);
+  }
+
+  /** Get an iterator */
+  iterator begin() { return iterator(head_); }
+  const_iterator begin() const { return const_iterator(head_); }
+  const_iterator cbegin() const { return const_iterator(head_); }
+  iterator end() { return iterator(); }
+  const_iterator end() const { return const_iterator(); }
+  const_iterator cend() const { return const_iterator(); }
+
+protected:
+  void push_front_(head_node* n) {
+    head_node* hn = *const_cast<head_node* volatile*>(&head_);
+    do {
+      n->next_ = hn;
+      hn = __sync_val_compare_and_swap (&head_, hn, n);
+    } while(hn != n->next_);
+  }
+
+public:
+  class iterator : public std::iterator<std::forward_iterator_tag, T> {
+  public:
+    explicit iterator(head_node* head = 0) : head_(head) { }
+    iterator(const iterator& rhs) : head_(rhs.head_) { }
+    iterator& operator=(const iterator& rhs) {
+      head_ = rhs.head_;
+      return *this;
+    }
+    bool operator==(const iterator& rhs) { return head_ == rhs.head_; }
+    bool operator!=(const iterator& rhs) { return head_ != rhs.head_; }
+    T& operator*() { return static_cast<node*>(head_)->val_; }
+    const T& operator*() const { return static_cast<const node*>(head_)->val_; }
+    T* operator->() { return &static_cast<node*>(head_)->val_; }
+    const T& operator->() const { return &static_cast<const node*>(head_)->val_; }
+    iterator& operator++() { head_ = (*const_cast<head_node* volatile*>(&head_))->next_; return *this; }
+    iterator operator++(int) {
+      iterator res(*this);
+      ++*this;
+      return res;
+    }
+  private:
+    head_node* head_;
+  };
+
+  class const_iterator : public std::iterator<std::forward_iterator_tag, T> {
+  public:
+    explicit const_iterator(head_node* head = 0) : head_(head) { }
+    const_iterator(const const_iterator& rhs) : head_(rhs.head_) { }
+    const_iterator(const iterator& rhs) : head_(rhs.head_) { }
+    const_iterator& operator=(const const_iterator& rhs) {
+      head_ = rhs.head_;
+      return *this;
+    }
+    bool operator==(const const_iterator& rhs) const { return head_ == rhs.head_; }
+    bool operator!=(const const_iterator& rhs) const { return head_ != rhs.head_; }
+    const T& operator*() const { return static_cast<const node*>(head_)->val_; }
+    const T& operator->() const { return &static_cast<const node*>(head_)->val_; }
+    const_iterator& operator++() { head_ = (*const_cast<head_node* volatile*>(&head_))->next_; return *this; }
+    const_iterator operator++(int) {
+      const_iterator res(*this);
+      ++*this;
+      return res;
+    }
+  private:
+    head_node* head_;
+  };
+
+private:
+  typedef typename Alloc::template rebind<T>::other    T_alloc_type;
+  typedef typename Alloc::template rebind<node>::other node_alloc_type;
+  T_alloc_type    T_alloc_;
+  node_alloc_type node_alloc_;
+  head_node*      head_;
+};
+
+#endif /* _LF_FORWARD_LIST_HPP_ */
---
+++ tests/test_lf_forward_list.cc
@@ -0,0 +1,28 @@
+#include <gtest/gtest.h>
+#include <lf_forward_list.hpp>
+
+namespace {
+TEST(LfForwardList, PushFront) {
+  static const int size = 10000;
+  lf_forward_list<int> list;
+
+  EXPECT_TRUE(list.cbegin() == list.cend());
+  EXPECT_FALSE(list.cbegin() != list.cend());
+
+  #pragma omp parallel for
+  for(int i = 0; i < size; ++i)
+    list.push_front(i);
+
+  EXPECT_FALSE(list.cbegin() == list.cend());
+  EXPECT_TRUE(list.cbegin() != list.cend());
+
+  std::set<int> unique;
+  for(auto it = list.cbegin(); it != list.cend(); ++it) {
+    auto res = unique.insert(*it);
+    EXPECT_TRUE(res.second); // Means it was not present in the set
+    EXPECT_TRUE(*it >= 0 && *it < size);
+    std::cerr << *it << "\n";
+  }
+  EXPECT_EQ(size, unique.size());
+}
+}
