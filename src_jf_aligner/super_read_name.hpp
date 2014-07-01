35cb62025d089782019b0cb2ea58a279c15c8930
Guillaume Marcais <Guillaume Marcais guillaume@marcais.net>
2014-07-01 15:28:06 -0400

    super_read_name represented as vector of unitig id instead of the string name. Implemented overlap method.

--- src_jf_aligner/super_read_name.hpp
+++ src_jf_aligner/super_read_name.hpp
@@ -2,73 +2,97 @@
 #define __SUPER_READ_NAME_H__
 
 #include <limits>
+#include <algorithm>
+#include <debug.hpp>
 
 class super_read_name {
-  const std::string         name_;
-  const std::vector<size_t> unitigs_;
+  const std::vector<long> unitigs_;
 
 public:
   static const unsigned long invalid = std::numeric_limits<unsigned long>::max();
 
-  super_read_name(const std::string& name) : name_(name), unitigs_(parse(name)) { }
+  super_read_name(const std::string& name) : unitigs_(parse(name)) { }
+  super_read_name(std::vector<long>&& unitigs) : unitigs_(std::move(unitigs)) { }
 
   struct unitig {
     unsigned long id;
     char          ori; // 'F' or 'R'
   };
 
-  size_t nb_unitigs() const { return unitigs_.size() - 1; }
+  size_t nb_unitigs() const { return unitigs_.size(); }
   size_t size() const { return nb_unitigs(); }
-  const std::string& name() const { return name_; }
-
-  unitig operator[](size_t i) const {
-    if(i < nb_unitigs())
-      return { std::stoul(name_.substr(unitigs_[i], unitigs_[i + 1] - unitigs_[i] - 2)), name_[unitigs_[i + 1] - 2] };
-    return { invalid, '\0' };
-  }
-
-  unsigned long unitig_id(size_t i) const {
-    return this->operator[](i).id;
-  }
-
-  static char reverse_ori(char ori) {
-    switch(ori) {
-    case 'F': return 'R';
-    case 'R': return 'F';
-    default: return ori; // Hum?? Bad orientation to begin with.
-    }
-  }
-
-  std::string reverse() const {
+  std::string unitig_name(long i) const { return std::to_string(abs(i)) + (i > 0 ? 'F' : 'R'); }
+  std::string name() const {
     std::string res;
-    if(nb_unitigs() > 0) {
-      for(size_t i = nb_unitigs(); i >= 1; --i) {
-        res += name_.substr(unitigs_[i - 1], unitigs_[i] - unitigs_[i - 1] - 2);
-        res += reverse_ori(name_[unitigs_[i] - 2]);
-        if(i > 1) res += '_';
-      }
+    auto it = unitigs_.cbegin();
+    if(it != unitigs_.cend()) {
+      res += unitig_name(*it);
+      for(++it; it != unitigs_.cend(); ++it)
+        (res += '_') += unitig_name(*it);
     }
     return res;
   }
 
-  // // Return the length of the longest overlap by unitigs between two
-  // // super reads, in a dovetail fashion. The return value is the
-  // // largest integer m such that the last m k-unitigs of *this are
-  // // equal to the first m k-unitigs of rhs. Note that this
-  // // relationship is NOT symmetrical, i.e. *this overlaps with rhs and
-  // // comes before rhs.
-  // int overlap(const super_read_name& rhs) {
-    
+  unitig operator[](size_t i) const {
+    if(i < nb_unitigs())
+      return { (unsigned long)abs(unitigs_[i]), unitigs_[i] > 0 ? 'F' : 'R' };
+    return { invalid, '\0' };
+  }
+
+  unsigned long unitig_id(size_t i) const { return i < nb_unitigs() ? abs(unitigs_[i]) : invalid; }
+
+  // static char reverse_ori(char ori) {
+  //   switch(ori) {
+  //   case 'F': return 'R';
+  //   case 'R': return 'F';
+  //   default: return ori; // Hum?? Bad orientation to begin with.
+  //   }
   // }
 
+  super_read_name reverse() const {
+    const size_t n = unitigs_.size();
+    std::vector<long> us(n, 0);
+    for(size_t i = 0; i < us.size() / 2; ++i) {
+      us[i]         = -unitigs_[n - i - 1];
+      us[n - i - 1] = -unitigs_[i];
+    }
+    if(n % 2 == 1)
+      us[n / 2] = -unitigs_[n / 2];
+    return super_read_name(std::move(us));
+  }
+
+  // Return the length of the longest overlap by unitigs between two
+  // super reads, in a dovetail fashion. The return value is the
+  // largest integer m such that the last m k-unitigs of *this are
+  // equal to the first m k-unitigs of rhs. Note that this
+  // relationship is NOT symmetrical, i.e. if *this overlaps with rhs
+  // then it comes before rhs. If the return is 0, there is no
+  // overlap.
+  int overlap(const super_read_name& rhs) const {
+    if(rhs.unitigs_.empty()) return 0;
+    const long fu = rhs.unitigs_.front();
+
+    for(auto it = std::find(unitigs_.cbegin(), unitigs_.cend(), fu);
+        it != unitigs_.cend();
+        it = std::find(it + 1, unitigs_.cend(), fu)) {
+      const int olen = unitigs_.cend() - it;
+      if(olen <= rhs.unitigs_.size() && std::equal(it, unitigs_.cend(), rhs.unitigs_.cbegin()))
+        return olen;
+    }
+    return 0;
+  }
+
 protected:
-  static std::vector<size_t> parse(std::string name) {
-    std::vector<size_t> res;
-    res.push_back(0);
+  static std::vector<long> parse(std::string name) {
+    std::vector<long> res;
     if(!name.empty()) {
-      for(size_t n = name.find_first_of('_'); n != std::string::npos; n = name.find_first_of('_', n + 1))
-        res.push_back(n + 1);
-      res.push_back(name.size() + 1);
+      size_t pn = 0;
+      for(size_t n = name.find_first_of('_'); n != std::string::npos; pn = n + 1, n = name.find_first_of('_', pn)) {
+        long id = std::stoul(name.c_str() + pn);
+        res.push_back(name[n - 1] == 'R' ? -id : id);
+      }
+      long id = std::stoul(name.c_str() + pn);
+      res.push_back(name[name.size() - 1] == 'R' ? -id : id);
     }
     return res;
   }
--- tests/test_super_read_name.cc
+++ tests/test_super_read_name.cc
@@ -5,7 +5,7 @@ namespace {
 TEST(SuperReadName, Parse) {
   super_read_name n1("");
   EXPECT_EQ((size_t)0, n1.nb_unitigs());
-  EXPECT_EQ("", n1.reverse());
+  EXPECT_EQ("", n1.reverse().name());
   {
     const auto u = n1[0];
     EXPECT_EQ(std::numeric_limits<unsigned long>::max(), u.id);
@@ -15,7 +15,7 @@ TEST(SuperReadName, Parse) {
 
   super_read_name n2("1234F");
   EXPECT_EQ((size_t)1, n2.nb_unitigs());
-  EXPECT_EQ("1234R", n2.reverse());
+  EXPECT_EQ("1234R", n2.reverse().name());
   {
     const auto u = n2[0];
     EXPECT_EQ((unsigned long)1234, u.id);
@@ -50,5 +50,25 @@ TEST(SuperReadName, Parse) {
     EXPECT_EQ(u.id, n3.unitig_id(nb));
   }
 }
+
+TEST(SuperReadName, Overlap) {
+  super_read_name empty("");
+  super_read_name sr1("1F_2R_3F_4R");
+  super_read_name sr2("4R_5F_6R");
+  super_read_name sr2r("4F_5R_6F");
+  super_read_name sr3("1F_2R_7F_1F_2R");
+  super_read_name sr4("2R");
+
+  EXPECT_EQ(0, empty.overlap(empty));
+  EXPECT_EQ(0, empty.overlap(sr1));
+  EXPECT_EQ(0, sr1.overlap(empty));
+  EXPECT_EQ(1, sr1.overlap(sr2));
+  EXPECT_EQ(0, sr2.overlap(sr1));
+  EXPECT_EQ(0, sr1.overlap(sr2r));
+  EXPECT_EQ(0, sr2r.overlap(sr1));
+  EXPECT_EQ(2, sr3.overlap(sr1));
+  EXPECT_EQ(1, sr3.overlap(sr4));
+  EXPECT_EQ(0, sr3.overlap(sr2));
+} // SuperReadName.Overlap
 
 } // empty namespace
