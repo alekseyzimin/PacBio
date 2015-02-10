f7387d092e5d5ff7b10e469d7d1c7ca95fc61ff7
Guillaume Marcais <gmarcais@umd.edu>
2015-02-10 13:58:13 -0500

    Parse input sequence and create partial suffix array. Still buggy.

--- src_jf_aligner/superread_parser.cc
+++ src_jf_aligner/superread_parser.cc
@@ -1,82 +1,119 @@
-#include <tuple>
+#include <thread>
+
 #include <src_jf_aligner/superread_parser.hpp>
+#include <src_psa/barrier.hpp>
+#include <src_psa/slice.hpp>
+#include <src_psa/compact_index.hpp>
+#include <src_psa/mer_sa_imp.hpp>
 
-void superreads_read_mers::start(int thid) {
-  if(ary_short_ == 0) {
-    parse_one_mer(thid);
-  } else {
-    parse_two_mers(thid);
-  }
-}
+void sequence_psa::append_fasta(std::istream& is) {
+  int c = is.peek();
+  if(is.peek() != '>')
+    throw std::runtime_error("Not in fasta format");
 
-void superreads_read_mers::parse_one_mer(int thid) {
-  parse_sequence            parser(compress_);
-  mer_pos_hash_type::thread at_long(*ary_long_);
+  size_t       seq_offset    = m_offsets.back().sequence;
+  size_t       search_offset = m_header_search.back();
+  std::string  line;
+  std::string  header_line;
+  for( ; c != EOF; c = is.peek()) {
+    // read header
+    std::getline(is, header_line);
 
-  while(true) {
-    read_parser::job job(parser_);
-    if(job.is_empty()) break;
+    // read sequence
+    size_t old_seq_offset = seq_offset;
+    for(c = is.peek(); c != '>' && c != EOF; c = is.peek()) {
+      std::getline(is, line);
+      if(m_sequence.size() * sizeof(uint64_t) * 4 < line.size() + seq_offset)
+        m_sequence.resize(std::max(1 + (size_t)(line.size() + seq_offset) / (sizeof(uint64_t) * 4), m_sequence.size() * 2));
+      compact_dna::copy_from_str(compact_dna::iterator(m_sequence.data(), 2, 0) + seq_offset, line);
+      // TODO: Should we check that everything was copied?
+      seq_offset += line.size();
+    }
 
-    for(size_t i = 0; i < job->nb_filled; ++i) { // Process each read
-      auto name_end = job->data[i].header.find_first_of(" \t\n\v\f\r");
-      auto header   = names_.push_back(thid, job->data[i].seq.length(), job->data[i].header.substr(0, name_end));
-      parser.reset(job->data[i].seq);
-
-      while(parser.next()) { // Process each k-mer
-        if(!parser.mer<0>().m.is_homopolymer()) {
-          const bool is_canonical = parser.mer<0>().is_canonical();
-          at_long.push_front(is_canonical ? parser.mer<0>().m : parser.mer<0>().rm,
-                             header,
-                             is_canonical ? parser.offset<0>() : -parser.offset<0>());
-        }
-      }
+    if(seq_offset > old_seq_offset) {
+      m_headers.push_back(frag_lists::frag_info(seq_offset - old_seq_offset, header_line.c_str()));
+      const size_t next_search_offset = seq_offset >> m_search_bits;
+      for(size_t i = search_offset + 1; i <= next_search_offset; ++i)
+        m_header_search.push_back(m_offsets.size() - 1);
+      search_offset = next_search_offset;
+      m_offsets.push_back({ m_headers.size(), seq_offset });
     }
   }
 }
 
-void superreads_read_mers::parse_two_mers(int thid) {
-  parse_sequence2                 parser(compress_);
-  mer_pos_hash_type::thread       at_long(*ary_long_);
-  short_mer_pos_hash_type::thread at_short(*ary_short_);
+static void
+create_thread(int thid, int nb_threads, barrier<std::mutex>* thread_barrier, slice_for<size_t, barrier<std::mutex>>* slicer,
+              compact_dna::const_iterator T, compact_index<uint64_t>::iterator SA,
+              const sequence_psa::offsets_type& offsets,
+              size_t n, uint64_t* mer_counts, unsigned int mer_size, unsigned int max_size) {
+  const size_t nb_counts    = (1 << (2 * mer_size)) + 1;
+  const size_t counts_mask  = ~(size_t)0 >> (8 * sizeof(size_t) - 2 * mer_size);
+  const size_t counts_start = (nb_counts - 1) * thid / nb_threads;
+  const size_t counts_end   = (nb_counts - 1) * (thid + 1) / nb_threads;
+  //  const size_t n_step       = std::min((size_t)(1024 * 1024), std::max((size_t)1, n / nb_threads));
 
-  while(true) {
-    read_parser::job job(parser_);
-    if(job.is_empty()) break;
+  // Add 1 to length of zeroing if thid == nb_threads because
+  // counts_end is at most nb_counts - 1, and need to zero up to
+  // nb_counts.
+  std::fill_n(mer_counts + counts_start, counts_end - counts_start + (thid == nb_threads), (uint64_t)0);
 
-    for(size_t i = 0; i < job->nb_filled; ++i) { // Process each read
-      auto name_end = job->data[i].header.find_first_of(" \t\n\v\f\r");
-      auto header   = names_.push_back(thid, job->data[i].seq.length(), job->data[i].header.substr(0, name_end));
-      parser.reset(job->data[i].seq);
-
-      while(parser.next()) { // Process each k-mer
-        if(parser.mer<0>().valid && !parser.mer<0>().m.is_homopolymer()) {
-          const bool is_canonical = parser.mer<0>().is_canonical();
-          at_long.push_front(is_canonical ? parser.mer<0>().m : parser.mer<0>().rm,
-                             header,
-                             is_canonical ? parser.offset<0>() : -parser.offset<0>());
-        }
-        if(parser.mer<0>().valid && !parser.mer<1>().m.is_homopolymer()) {
-          const bool is_canonical = parser.mer<1>().is_canonical();
-          at_short.push_front(is_canonical ? parser.mer<1>().m : parser.mer<1>().rm,
-                              header,
-                              is_canonical ? parser.offset<1>() : -parser.offset<1>());
-        }
-      }
-    }
+  {
+    std::unique_ptr<uint64_t[]> tmp_counts(new uint64_t[nb_counts - 1]);
+    std::fill_n(tmp_counts.get(), nb_counts - 1, (uint64_t)0);
+    slicer->loop(0, offsets.size() - 1, 10, [=](size_t i) {
+        sequence_psa::SA::count_mers(T + offsets[i].sequence,
+                                     offsets[i + 1].sequence - offsets[i].sequence,
+                                     mer_counts, mer_size);
+      });
+    for(size_t i = 0, j = counts_start; i < nb_counts - 1; ++i, j = ((j + 1) & counts_mask))
+      __sync_fetch_and_add(mer_counts + j, tmp_counts[j]);
   }
+
+  if(thread_barrier->wait())
+    sequence_psa::SA::partial_sums(mer_counts, nb_counts);
+
+  typedef typename compactsufsort_imp::parallel_iterator_traits<compact_index<uint64_t>::iterator>::type PSAIDPTR;
+  PSAIDPTR PSA(SA);
+  slicer->loop(0, offsets.size() - 1, 10, [=](size_t i) {
+    sequence_psa::SA::fill_mers(T, offsets[i].sequence, offsets[i + 1].sequence, PSA, mer_counts, mer_size);
+    });
+
+  slicer->loop(0, nb_counts - 1, 100, [=](size_t i) {
+      sequence_psa::SA::sort_one_mer(T, n, PSA, mer_counts[i], mer_counts[i + 1], mer_size, max_size);
+    });
 }
 
-void superread_parse(int threads, mer_pos_hash_type& hash, frag_lists& names,
-                     file_vector::const_iterator begin, file_vector::const_iterator end,
-                     bool compress) {
-  names.ensure(threads);
-  stream_manager streams(begin, end);
-  superreads_read_mers reader(threads, &hash, 0, names, streams, compress);
-  reader.exec_join(threads);
+
+void sequence_psa::compute_psa(unsigned int min_size, unsigned int max_size, unsigned int threads) {
+  m_min_size = min_size;
+  m_max_size = max_size;
+
+  barrier<std::mutex>      SA_barrier(threads);
+  slice_for<size_t, barrier<std::mutex>> slicer(SA_barrier);
+  std::vector<std::thread> thread_handles;
+  m_counts.resize((1 << (2 * min_size)) + 1);
+  m_sa.reset(new compact_index<uint64_t>(nb_mers()));
+
+  for(unsigned int i = 0; i < threads; ++i)
+    thread_handles.push_back(std::thread(create_thread, i, threads, &SA_barrier, &slicer,
+                                         compact_dna::const_iterator_at(m_sequence.data()),
+                                         m_sa->begin(), m_offsets,
+                                         sequence_size(), m_counts.data(), min_size, max_size));
+  for(unsigned int i = 0; i < threads; ++i)
+    thread_handles[i].join();
 }
 
-void superread_parse(int threads, mer_pos_hash_type& hash, frag_lists& names, const char* file, bool compress) {
-  file_vector files;
-  files.push_back(file);
-  superread_parse(threads, hash, names, files.cbegin(), files.cend(), compress);
-}
+// void superread_parse(int threads, mer_pos_hash_type& hash, frag_lists& names,
+//                      file_vector::const_iterator begin, file_vector::const_iterator end,
+//                      bool compress) {
+//   names.ensure(threads);
+//   stream_manager streams(begin, end);
+//   superreads_read_mers reader(threads, &hash, 0, names, streams, compress);
+//   reader.exec_join(threads);
+// }
+
+// void superread_parse(int threads, mer_pos_hash_type& hash, frag_lists& names, const char* file, bool compress) {
+//   file_vector files;
+//   files.push_back(file);
+//   superread_parse(threads, hash, names, files.cbegin(), files.cend(), compress);
+// }
