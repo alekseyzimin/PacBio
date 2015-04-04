9ca1049bc0baf7f3ae6fcf8320187a4baa99876c
Aleksey Zimin <alekseyz@ipst.umd.edu>
2015-04-04 09:26:15 -0400

    updated the run scripts

--- src_mega_reads/mega_reads_assemble.sh
+++ src_mega_reads/mega_reads_assemble.sh
@@ -1,31 +1,121 @@
 #!/bin/bash
-#!/bin/bash
 MYPATH="`dirname \"$0\"`"
 MYPATH="`( cd \"$MYPATH\" && pwd )`"
-export PATH=$MYPATH:/home/alekseyz/myprogs/masurca-devel/build/inst/bin/:$MYPATH/../build-default:$MYPATH/../build-default/src_jf_aligner:$MYPATH/../build-default/src_mega_reads:/home/alekseyz/wgs-8.2/Linux-amd64/bin/:$PATH
-#arguments
-COORDS=$1
-KMER=$2
-KUNITIGS=$3
-KUNITIGLENGTHS=$4
-SUPERREADS=$5
-PACBIO=$6
-JF_SIZE=$7
+#parsing arguments
+while [[ $# > 0 ]]
+do
+key="$1"
 
-#parameters
+case $key in
+    -m|--masurca_run_path)
+    MASURCA_ASSEMBLY_WORK1_PATH="$2"
+    shift
+    ;;
+    -p|--pacbio)
+    PACBIO="$2"
+    shift
+    ;;
+    -a|--assembler_path)
+    CA_PATH="$2"
+    shift
+    ;;
+    -v|--verbose)
+    set -x
+    ;;
+    -h|--help|-u|--usage)
+    echo "Usage: mega_reads_assemble.sh -m <path to MaSuRCA run work1 folder -p <pacbio reads fasta> -a <path to the location of runCA in wgs-8.2 instalation>"
+    exit 1
+    ;;
+    *)
+    echo "Unknown option $1"
+    exit 1        # unknown option
+    ;;
+esac
+shift
+done
+
+###############checking arguments#########################
+if [ ! -e $CA_PATH/runCA ];then
+echo "runCA not found at $CA_PATH!";
+exit 1;
+fi
+
+export PATH=$MYPATH:/home/alekseyz/myprogs/masurca-devel/build/inst/bin/:$MYPATH/../build-default:$MYPATH/../build-default/src_jf_aligner:$MYPATH/../build-default/src_mega_reads:$CA_PATH:$PATH
+
+if [ ! -e $PACBIO ];then
+echo "PacBio reads file $PACBIO not found!";
+exit 1 ;
+fi
+
+
+KUNITIGS=$MASURCA_ASSEMBLY_WORK1_PATH/../guillaumeKUnitigsAtLeast32bases_all.fasta
+if [ ! -e $KUNITIGS ];then
+echo "K-unitigs file $KUNITIGS not found!";
+exit 1;
+fi
+
+KUNITIGLENGTHS=$MASURCA_ASSEMBLY_WORK1_PATH/kUnitigLengths.txt
+if [ ! -e $KUNITIGLENGTHS ];then
+echo "K-unitig lengths file $KUNITIGLENGTHS not found!";
+exit 1;
+fi
+
+SUPERREADS=$MASURCA_ASSEMBLY_WORK1_PATH/superReadSequences.fasta
+if [ ! -e $SUPERREADS ];then
+echo "Super reads file $SUPERREADS not found!";
+exit 1;
+else
+if [ ! -e superReadSequences.named.fasta ];then
+perl -ane 'push(@names,$F[0]);
+	END{
+	open(FILE,"'$SUPERREADS'");
+	while($line=<FILE>){
+		if($line=~/^>/){
+			chomp($line);
+			print ">",$names[substr($line,1)],"\n";
+		}else{
+			print $line;
+	}
+	}
+}' < $MASURCA_ASSEMBLY_WORK1_PATH/superReadNames.txt > superReadSequences.named.fasta.tmp && mv superReadSequences.named.fasta.tmp superReadSequences.named.fasta
+fi
+if [ -s superReadSequences.named.fasta ];then
+SUPERREADS=superReadSequences.named.fasta;
+else
+echo "Error creating named super-reads file from $MASURCA_ASSEMBLY_WORK1_PATH/superReadNames.txt and $SUPERREADS!";
+rm superReadSequences.named.fasta
+exit 1;
+fi
+fi
+
+################setting parameters#########################
 MER=15
 B=17
 d=0.03
-NUM_THREADS=48
-
-COORDS=$COORDS.$KMER.$MER.$B.$d
+KMER=`perl -ane 'BEGIN{$min=10000}{if($F[1]<$min){$min=$F[1]}}END{print $min}' $KUNITIGLENGTHS`
+NUM_THREADS=`cat /proc/cpuinfo |grep ^processor |wc -l`
+REF_BATCH_SIZE=`grep -v '^>' $PACBIO |wc| perl -ane '{$s=int($F[2]/500);$s=100000000 if($s>100000000); print $s;}'`
+QRY_BATCH_SIZE=500000000
+JF_SIZE=`grep -v '^>' $SUPERREADS |wc| perl -ane '{print $F[2]}'`
+COORDS=mr.$KMER.$MER.$B.$d
 CA=CA.${COORDS}
 
-if [ ! -e $COORDS.blasr.out ];then
+echo "Running mega-reads correction/assembly"
+echo "Using mer size $MER for mapping, B=$B, d=$d"
+echo "Using MaSuRCA files from $MASURCA_ASSEMBLY_WORK1_PATH, k-unitig mer $KMER"
+echo "Using CA installation from $CA_PATH"
+echo "Using $NUM_THREADS threads"
+echo "Output prefix $COORDS"
 
-create_mega_reads -s $JF_SIZE -m $MER --stretch-cap 10000 -k $KMER -u $KUNITIGS -t $NUM_THREADS -B $B --max-count 300 -d $d  -r $SUPERREADS  -p $PACBIO -o $COORDS.txt
+rm -f .rerun
 
-#first we reduce to maximal
+if [ ! -s $COORDS.txt ] || [ -e .rerun ];then
+echo "Mega-reads pass 1"
+create_mega_reads -s $JF_SIZE -m $MER --stretch-cap 10000 -k $KMER -u $KUNITIGS -t $NUM_THREADS -B $B --max-count 300 -d $d  -r $SUPERREADS  -p $PACBIO -o $COORDS.txt.tmp && mv $COORDS.txt.tmp $COORDS.txt
+touch .rerun
+fi
+
+if [ ! -s $COORDS.all_mr.fa ] || [ -e .rerun ];then
 perl -ane '{
 if($F[0] =~ /^\>/){
 $pb=substr($F[0],1);
@@ -44,10 +134,17 @@ print ">$mega_read\n$sequence\n";
 $out{$mega_read}=1;
 }
 }
-}' $COORDS.txt 1> $COORDS.all_mr.fa 
+}' $COORDS.txt 1> $COORDS.all_mr.fa.tmp && mv $COORDS.all_mr.fa.tmp $COORDS.all_mr.fa 
+touch .rerun
+fi
 
-create_mega_reads -s $JF_SIZE -m $MER -k $KMER -u $KUNITIGS -t $NUM_THREADS -B $B --max-count 300 -d 0.05  -r $COORDS.all_mr.fa  -p $PACBIO -o $COORDS.mr.txt
+if [ ! -s $COORDS.mr.txt ] || [ -e .rerun ];then
+echo "Mega-reads pass 2"
+create_mega_reads -s $JF_SIZE -m $MER -k $KMER -u $KUNITIGS -t $NUM_THREADS -B $B --max-count 300 -d 0.05  -r $COORDS.all_mr.fa  -p $PACBIO -o $COORDS.mr.txt.tmp && mv $COORDS.mr.txt.tmp $COORDS.mr.txt
+touch .rerun
+fi
 
+if [ ! -s $COORDS.all_mr.mr.fa ] || [ -e .rerun ];then
 perl -ane '{
 if($F[0] =~ /^\>/){
 $pb=substr($F[0],1);
@@ -66,11 +163,14 @@ print ">$mega_read\n$sequence\n";
 $out{$mega_read}=1;
 }
 }
-}' $COORDS.mr.txt 1> $COORDS.all_mr.mr.fa 
+}' $COORDS.mr.txt 1> $COORDS.all_mr.mr.fa.tmp  && mv $COORDS.all_mr.mr.fa.tmp $COORDS.all_mr.mr.fa
+touch .rerun
+fi
 
-
+if [ ! -s $PACBIO.$COORDS.maximal_mr.fa.g.delta ] || [ -e .rerun ];then
+echo "Maximal alignment"
 perl -ane  '{if($F[0] =~ /^\>/){print substr($F[0],1);}else{ print " ",length($F[0]),"\n";}}' $COORDS.all_mr.mr.fa | sort -nrk2 -S 10%  > $COORDS.mr_sizes.tmp
-reduce_sr `wc -l $4 | perl -ane 'print $F[0]'`  $KUNITIGLENGTHS $KMER $COORDS.mr_sizes.tmp -o $COORDS.reduce.tmp
+reduce_sr `wc -l $KUNITIGLENGTHS | perl -ane 'print $F[0]'`  $KUNITIGLENGTHS $KMER $COORDS.mr_sizes.tmp -o $COORDS.reduce.tmp
 cat <(awk '{print $1}' $COORDS.reduce.tmp) <(awk '{print $1}'  $COORDS.mr_sizes.tmp) | sort -S 10% |uniq -u > $COORDS.maximal_mr.txt
 extractreads.pl $COORDS.maximal_mr.txt $COORDS.all_mr.mr.fa 1 | perl -ane 'BEGIN{$mr_number=0;}{
 if($F[0] =~ /^\>/){
@@ -87,32 +187,52 @@ print STDERR "$mega_read\n";
 $mr_number++;
 }
 }' 1>$COORDS.maximal_mr.fa 2>$COORDS.maximal_mr.names
-
-run_big_nucmer_job_parallel.sh $PACBIO $COORDS.maximal_mr.fa 1000000 200000000 '-d 0.2 -g 200 -l 15 -b 120 -c 100' $NUM_THREADS
-delta-filter -g -o 20 $PACBIO.$COORDS.maximal_mr.fa.g.delta > $PACBIO.$COORDS.maximal_mr.fa.gg.delta
-show-coords -lcHr  $PACBIO.$COORDS.maximal_mr.fa.gg.delta | awk '{if($4<$5){print $18"/0_"$12" "$19" 0 0 0 "$10" "$4" "$5" "$13" "$1" "$2" "$12" 0"}else{print $18"/0_"$12" "$19+1" 0 0 0 "$10" "$13-$4+1" "$13-$5+1" "$13" "$1" "$2" "$12" 0"}}' > $COORDS.blasr.out
-
-reconciliate_mega_reads.maximal.nucmer.pl 20 $KMER $COORDS.maximal_mr.fa $COORDS.maximal_mr.names < $COORDS.blasr.out 1> $COORDS.all.txt 2>$COORDS.blasr.merged
-
-findGapsInCoverageOfPacbios --max-gap-overlap 100  -f $COORDS.blasr.merged > $COORDS.bad_pb.txt
-
-analyze_mega_gaps.sh $COORDS  $KMER | determineUnjoinablePacbioSubmegas.perl --min-range-proportion 0.15 --min-range-radius 15 > ${COORDS}.1.allowed
-
+if [ -e .rerun ];then
+rm -rf tmp.nucmer.$PACBIO.$COORDS.maximal_mr.fa;
+rm -rf nucmer.$PACBIO.$COORDS.maximal_mr.fa;
+fi
+run_big_nucmer_job_parallel.sh $PACBIO $COORDS.maximal_mr.fa $REF_BATCH_SIZE $QRY_BATCH_SIZE '--maxmatch -d 0.2 -g 200 -l 15 -b 120 -c 100' $NUM_THREADS
+touch .rerun
 fi
 
-join_mega_reads_trim.onepass.pl $PACBIO ${COORDS}.1.allowed $KMER $COORDS.bad_pb.txt < ${COORDS}.all.txt > $COORDS.1.fa;
+if [ ! -s $COORDS.blasr.out ] || [ -e .rerun ];then
+echo "Alignments filtering"
+delta-filter -g -o 20 $PACBIO.$COORDS.maximal_mr.fa.g.delta > $PACBIO.$COORDS.maximal_mr.fa.gg.delta && show-coords -lcHr  $PACBIO.$COORDS.maximal_mr.fa.gg.delta | awk '{if($4<$5){print $18"/0_"$12" "$19" 0 0 0 "$10" "$4" "$5" "$13" "$1" "$2" "$12" 0"}else{print $18"/0_"$12" "$19+1" 0 0 0 "$10" "$13-$4+1" "$13-$5+1" "$13" "$1" "$2" "$12" 0"}}' > $COORDS.blasr.out.tmp && mv $COORDS.blasr.out.tmp $COORDS.blasr.out
+touch .rerun
+fi
 
-fasta2frg.pl mr 600 < $COORDS.1.fa > $COORDS.1.frg;
-#perl -ane 'BEGIN{$n=0}{if($F[0]=~/^>/){print ">$n\n";$n++}else{print "$F[0]\n"}}' $SUPERREADS | fasta2frg.pl sr  > $COORDS.sr.frg;
+if [ ! -s $COORDS.all.txt ] || [ -e .rerun ];then
+echo "Tiling"
+reconciliate_mega_reads.maximal.nucmer.pl 20 $KMER $COORDS.maximal_mr.fa $COORDS.maximal_mr.names < $COORDS.blasr.out 1> $COORDS.all.txt.tmp 2>$COORDS.blasr.merged && mv $COORDS.all.txt.tmp $COORDS.all.txt
+touch .rerun
+fi
 
-make_mate_frg.pl < $COORDS.1.fa > $COORDS.1.mates.frg
+if [ ! -s $COORDS.1.fa ] || [ -e .rerun ];then
+echo "Joining"
+findGapsInCoverageOfPacbios --max-gap-overlap 100  -f $COORDS.blasr.merged > $COORDS.bad_pb.txt.tmp && mv $COORDS.bad_pb.txt.tmp $COORDS.bad_pb.txt
+analyze_mega_gaps.sh $COORDS  $KMER | determineUnjoinablePacbioSubmegas.perl --min-range-proportion 0.15 --min-range-radius 15 > ${COORDS}.1.allowed.tmp && mv ${COORDS}.1.allowed.tmp ${COORDS}.1.allowed
+echo "Generating assembly input files"
+join_mega_reads_trim.onepass.pl $PACBIO ${COORDS}.1.allowed $KMER $COORDS.bad_pb.txt < ${COORDS}.all.txt > $COORDS.1.fa.tmp && mv $COORDS.1.fa.tmp $COORDS.1.fa
+touch .rerun
+fi
 
-runCA unitigger=bogart  merylMemory=8192 utgGraphErrorLimit=1000  utgMergeErrorLimit=1000 utgGraphErrorRate=0.04 utgMergeErrorRate=0.04 ovlCorrBatchSize=5000 ovlCorrConcurrency=$NUM_THREADS frgCorrThreads=$NUM_THREADS mbtThreads=$NUM_THREADS ovlThreads=2 ovlHashBlockLength=100000000 ovlRefBlockSize=1000 ovlConcurrency=$NUM_THREADS doFragmentCorrection=1 doOverlapBasedTrimming=1 doUnitigSplitting=0 doChimeraDetection=normal doUnitigSplitting=0 stopAfter=unitigger cnsMinFrags=500 cnsConcurrency=32 -p genome -d $CA  merylThreads=$NUM_THREADS utgErrorLimit=1000 $COORDS.1.frg    $COORDS.1.mates.frg  1> $CA.log 2>&1
+if [ -e .rerun ];then
+fasta2frg.pl mr 600 < $COORDS.1.fa > $COORDS.1.frg.tmp && mv  $COORDS.1.frg.tmp  $COORDS.1.frg
+make_mate_frg.pl < $COORDS.1.fa > $COORDS.1.mates.frg.tmp && mv $COORDS.1.mates.frg.tmp $COORDS.1.mates.frg
+rm -rf $CA
+fi
 
-#echo "preliminary stats:"
-#tigStore -g $CA/genome.gkpStore -t $CA/genome.tigStore 2 -U -d sizes -s 12000000
+rm -f .rerun
 
-runCA cnsReuseUnitigs=1 cgwMergeMissingThreshold=-1 cgwMergeFilterLevel=1 cgwDemoteRBP=0 cgwErrorRate=0.25 merylMemory=8192 utgGraphErrorLimit=1000 utgGraphErrorRate=0.035 utgMergeErrorLimit=1000 utgMergeErrorRate=0.045 ovlCorrBatchSize=5000 ovlCorrConcurrency=$NUM_THREADS frgCorrThreads=$NUM_THREADS mbtThreads=$NUM_THREADS ovlThreads=2 ovlHashBlockLength=100000000 ovlRefBlockSize=10000 ovlConcurrency=$NUM_THREADS doFragmentCorrection=1 doOverlapBasedTrimming=1 doUnitigSplitting=0 doChimeraDetection=normal doUnitigSplitting=0 cnsMinFrags=300 cnsConcurrency=32 -p genome -d $CA unitigger=bogart merylThreads=$NUM_THREADS utgErrorLimit=1000 $COORDS.1.frg  1>> $CA.log 2>&1
+echo "Running assembly"
+
+runCA unitigger=bogart  merylMemory=8192 utgGraphErrorLimit=1000  utgMergeErrorLimit=1000 utgGraphErrorRate=0.04 utgMergeErrorRate=0.04 ovlCorrBatchSize=5000 ovlCorrConcurrency=$NUM_THREADS frgCorrThreads=$NUM_THREADS mbtThreads=$NUM_THREADS ovlThreads=2 ovlHashBlockLength=100000000 ovlRefBlockSize=10000 ovlConcurrency=$NUM_THREADS doFragmentCorrection=1 doOverlapBasedTrimming=1 doUnitigSplitting=0 doChimeraDetection=normal stopAfter=unitigger -p genome -d $CA  merylThreads=$NUM_THREADS utgErrorLimit=1000 $COORDS.1.frg    $COORDS.1.mates.frg  1> $CA.log 2>&1
+
+echo "Unitig stats:"
+tigStore -g $CA/genome.gkpStore -t $CA/genome.tigStore 2 -U -d sizes 
+
+runCA cnsReuseUnitigs=1 cgwMergeMissingThreshold=-1 cgwMergeFilterLevel=1 cgwDemoteRBP=0 cgwErrorRate=0.25  doFragmentCorrection=1 doOverlapBasedTrimming=1 doUnitigSplitting=0 doChimeraDetection=normal cnsMinFrags=300 cnsConcurrency=$NUM_THREADS -p genome -d $CA unitigger=bogart merylThreads=$NUM_THREADS utgErrorLimit=1000 $COORDS.1.frg  1>> $CA.log 2>&1
+
 
 #split_long_unitigs.pl cr < ${CA}/9-terminator/genome.ctg.fasta 2>assembly.${CA}.short_contigs.fa | fasta2frg.pl cr > assembly.${CA}.contig_reads.frg
 
--- src_mega_reads/run_big_nucmer_job_parallel.sh
+++ src_mega_reads/run_big_nucmer_job_parallel.sh
@@ -13,10 +13,11 @@ export PATH=$MYPATH:$PATH;
 
 export pid=$$;
 export rundir=/dev/shm/$pid.nucmer;
+export curdir=$PWD
 
 function cleanup {
 if [ $$ -eq $pid ];then
-rm -rf $rundir;
+rm -rf $rundir &
 fi
 }
 trap cleanup EXIT SIGHUP SIGINT SIGTERM
@@ -39,9 +40,9 @@ wait $pid1 $pid2
 reffilename=$(basename $refseq)
 qryfilename=$(basename $qryseq)
 
-(cd $rundir;parallel -j $num_cpus "nucmer $nuc_params -p deltafile.{1}.{2} {1} {2} 1>/dev/null 2>&1" ::: $reffilename.* ::: $qryfilename.* ;)
+mkdir -p tmp.nucmer.$refseq.$qryseq
+(cd $rundir;parallel -j $num_cpus "if [ ! -e $curdir/tmp.nucmer.$refseq.$qryseq/deltafile.{1}.{2}.delta ];then nucmer $nuc_params -p tmpdeltafile.{1}.{2} {1} {2} 1>/dev/null 2>&1 && mv tmpdeltafile.{1}.{2}.delta $curdir/tmp.nucmer.$refseq.$qryseq/deltafile.{1}.{2}.delta;fi" ::: $reffilename.* ::: $qryfilename.* && mv $curdir/tmp.nucmer.$refseq.$qryseq $curdir/nucmer.$refseq.$qryseq);
 
-head -n 2 $rundir/deltafile.$reffilename.1.$qryfilename.1.delta > $reffilename.$qryfilename.g.delta
-ls  $rundir/deltafile*.delta | xargs cat |grep -v NUCMER | grep -v  $reffilename >> $reffilename.$qryfilename.g.delta
-
-
+if [ -d nucmer.$refseq.$qryseq ];then
+head -n 2 nucmer.$refseq.$qryseq/deltafile.$reffilename.1.$qryfilename.1.delta > $reffilename.$qryfilename.g.delta.tmp && ls  nucmer.$refseq.$qryseq/deltafile*.delta | xargs cat |grep -v NUCMER | grep -v  $reffilename >> $reffilename.$qryfilename.g.delta.tmp && mv $reffilename.$qryfilename.g.delta.tmp $reffilename.$qryfilename.g.delta && rm -rf nucmer.$refseq.$qryseq
+fi
