7f652833eb2086e908ac5a0b41cb06d9b5055e1e
Aleksey Zimin <alekseyz@ipst.umd.edu>
2015-11-25 17:03:57 -0500

    seems to be correct and fast

--- src_mega_reads/mega_reads_assemble.sh
+++ src_mega_reads/mega_reads_assemble.sh
@@ -119,7 +119,7 @@ rm -f .rerun
 
 if [ ! -s $COORDS.txt ] || [ -e .rerun ];then
 echo "Mega-reads pass 1"
-create_mega_reads -s $JF_SIZE -m $MER --psa-min $MER --stretch-cap 10000 -k $KMER -u $KUNITIGS -t $NUM_THREADS -B $B --max-count 3000 -d $d  -r $SUPERREADS  -p $PACBIO -o $COORDS.txt.tmp && mv $COORDS.txt.tmp $COORDS.txt
+create_mega_reads -s $JF_SIZE -m $MER --psa-min 13 --stretch-cap 10000 -k $KMER -u $KUNITIGS -t $NUM_THREADS -B $B --max-count 3000 -d $d  -r $SUPERREADS  -p $PACBIO -o $COORDS.txt.tmp && mv $COORDS.txt.tmp $COORDS.txt
 touch .rerun
 fi
 
