31af2cd6c9751da1b02f03344e4cb3cf506f199f
Aleksey Zimin <alekseyz@ipst.umd.edu>
2016-03-14 15:30:32 -0400

    added two scripts for contig-scaffold reconcilitaion

---
+++ src_mega_reads/output_reconciled_scaffolds.pl
@@ -0,0 +1,43 @@
+#!/usr/bin/perl
+my $name="";
+my $seq="";
+my $seqfile=$ARGV[0];
+open(FILE,$seqfile);
+while($line=<FILE>){
+  chomp($line);
+  if($line=~/^\>/){
+    $sequence{$name}=$seq if(not($name eq ""));
+    ($name)=split(/\s+/,substr($line,1));
+    $seq="";
+  }else{
+    $seq=.$line;
+  }
+}
+
+$name="";
+$seq="";
+my $gap=1000000;
+while($line=<STDIN>){
+chomp($line);
+$gap=$gap<$f[5] ? $gap : $f[5];
+@f=split(/\s+/,$line);
+if(not($f[0] eq $name)){
+  print ">$name\n$seq\n";
+  $name=$f[0];
+  $seq="";
+}else{
+  $seq.=("N"x$gap);
+}
+$gap=$f[5];
+die("Sequence $f[1] not found") if(not(defined($sequence{$f[1]})));
+$seq.=substr($sequence{$f[1]},$f[2],$f[3]-$f[2]-1);
+}
+print ">$name\n$seq\n";
+
+sub reverse_complement{
+  my $str=$_[0];
+  $str =~ tr/acgtACGTNn/tgcaTGCANn/;
+  $str = reverse ($str);
+  return ($str);
+}
+
