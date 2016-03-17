026e844dd2e19fb61d04f2a87cf7ecafb8491063
Aleksey Zimin <alekseyz@ipst.umd.edu>
2016-03-17 15:23:57 -0400

    finalized the reconcile codes

--- src_mega_reads/reconcile_alignments.sh
+++ src_mega_reads/reconcile_alignments.sh
@@ -7,5 +7,4 @@ export PATH=$MYPATH:$PATH;
 COORDS_FILE=$1;
 SEQ_FILE=$2;
 
-merge_matches_and_tile_coords_file.pl 1000000 < $COORDS_FILE | awk 'BEGIN{last_end=0;last_scf="";}{if($18 != last_scf){last_end=0;last_scf=$18} if($2>last_end) print $0; last_end=$2}' |  reconcile_matches.pl 
-#| output_reconciled_scaffolds.pl $SEQ_FILE
+merge_matches_and_tile_coords_file.pl 1000000 < $COORDS_FILE | awk 'BEGIN{last_end=0;last_scf="";}{if($18 != last_scf){last_end=0;last_scf=$18} if($2>last_end) print $0; last_end=$2}' |  reconcile_matches.pl | output_reconciled_scaffolds.pl $SEQ_FILE
