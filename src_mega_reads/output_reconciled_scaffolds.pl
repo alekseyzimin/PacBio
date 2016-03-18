076a5a80749cabc1c9fb43cf64644038f29bdb3d
Aleksey Zimin <alekseyz@ipst.umd.edu>
2016-03-18 14:56:56 -0400

    more improvements to the reconcile -- if there is a sequence gap allow for more slack

--- src_mega_reads/output_reconciled_scaffolds.pl
+++ src_mega_reads/output_reconciled_scaffolds.pl
@@ -14,6 +14,7 @@ while($line=<FILE>){
     $seq.=$line;
   }
 }
+$sequence{$name}=$seq;
 
 $name="";
 $seq="";
--- src_mega_reads/reconcile_matches.pl
+++ src_mega_reads/reconcile_matches.pl
@@ -1,16 +1,26 @@
 #!/usr/bin/env perl
 
 
-my $tol_factor=10;#tolerance factor around gap size
-my $tol_min=100;#minimum tolerance for small gap
+my $tol_factor=30;#tolerance factor around gap size
+my $tol_min=500;#minimum tolerance for small gap
 #gap is an actual gap; all coordinates in the output are 1-based
+
+my $gap_coordinates=$ARGV[0];
+open(FILE,$gap_coordinates);
+
+while(my $line=<FILE>){
+chomp($line);
+my @f=split(/\s+/,$line);
+push(@{$gaps{$f[0]}},[$f[1],$f[2]]);
+}
+
 my $scf="";
-my $line=<>,@l=();
+my $line=<STDIN>,@l=();
 my @f=split(/\s+/,$line);
 my $scf=$f[-2];
 push(@l,$line);
 
-while($line=<>){
+while($line=<STDIN>){
   $line=~s/^\s+//;
   @f=split(/\s+/,$line);
   if(not($f[-2] eq $scf)){
@@ -33,22 +43,22 @@ sub process_lines{
     my @l1,@l2,@l3;
     @l2=split(/\s+/,$lines[0]);
     @l3=split(/\s+/,$lines[1]);
-    $gap_after=compute_gap($l2[1],$l3[0]);
+    $gap_after=compute_gap($l2[1],$l3[0],$l2[-2]);
     output_coords($gap_before,$gap_after,$l2[3],$l2[4],$l2[12],$l2[-2],$l2[-1],1);
     for($i=1;$i<$#lines;$i++){
       @l1=split(/\s+/,$lines[$i-1]);
       @l2=split(/\s+/,$lines[$i]);
       @l3=split(/\s+/,$lines[$i+1]);
 
-      $gap_before=compute_gap($l1[1],$l2[0]);
-      $gap_after=compute_gap($l2[1],$l3[0]);
+      $gap_before=compute_gap($l1[1],$l2[0],$l2[-2]);
+      $gap_after=compute_gap($l2[1],$l3[0],$l2[-2]);
 
       output_coords($gap_before,$gap_after,$l2[3],$l2[4],$l2[12],$l2[-2],$l2[-1],1);
     }
 
     @l1=split(/\s+/,$lines[$#lines-1]);
     @l2=split(/\s+/,$lines[$#lines]);
-    $gap_before=compute_gap($l1[1],$l2[0]);
+    $gap_before=compute_gap($l1[1],$l2[0],$l2[-2]);
     $gap_after=10000;
     output_coords($gap_before,$gap_after,$l2[3],$l2[4],$l2[12],$l2[-2],$l2[-1],0);
   }
@@ -57,6 +67,17 @@ sub process_lines{
 sub output_coords{
   my $start,$end,$dir;
   my ($gap_b,$gap_a,$s,$e,$len,$scf,$ctg)=@_;
+  my $sg_a=0,$sg_b=0;
+
+  if($gap_b<0){
+    $gap_b=-$gap_b;
+    $sg_b=1;
+    }
+  if($gap_a<0){
+    $gap_a=-$gap_a;
+    $sg_a=1;
+  }
+
   if($s<$e){#forward match
     $dir="f";
     if($s-1<=$gap_b){
@@ -86,21 +107,36 @@ sub output_coords{
       $end=$s;
     }
   }
-  $gap_a=$gap_a/$tol_factor;
-  $gap_b=$gap_b/$tol_factor;
+  $gap_a=$gap_a/$tol_factor if($sg_a);
+  $gap_b=$gap_b/$tol_factor if($sg_b);
 
   print "$scf $ctg $start $end $dir ",int($gap_b)," ",int($gap_a)," $len\n";
 }
 
 sub compute_gap{
-  my($gbeg,$gend)=@_;#gbeg<gend normally
-    if($gend-$gbeg==1){
-      return(0);
-    }elsif($gend-$gbeg<=101){
-      return(100*$tol_factor);
-    }else{
-      my $ttt=($gend-$gbeg)*$tol_factor;
-      return($ttt<$tol_min ? $tol_min*$tol_factor : $ttt);
+  my($gbeg,$gend,$name)=@_;#gbeg<gend normally
+  #check if this corresponds to a sequence gap
+  my $seq_gap=0;
+  foreach my $g(@{$gaps{$name}}){
+    if($$g[0]>=$gbeg && $$g[1]<=$gend){
+      $seq_gap=1;
+      last;
+    }elsif($g[0]>$gend){
+      last;
     }
+  }
+  if($seq_gap){
+    #print "found sequence gap $gbeg,$gend,$name\n";
+    my $ttt=($gend-$gbeg)*$tol_factor;
+    $ttt=$tol_min*$tol_factor if($ttt<$tol_min);
+    return(-$ttt); #negative gap is a flag for sequence gap
+  }elsif($gend-$gbeg==1){
+    return(0);
+  }elsif($gend-$gbeg<=101){
+    return(100);
+  }else{
+    my $ttt=($gend-$gbeg);
+    return($ttt<$tol_min ? $tol_min : $ttt);
+  }
 }
 
