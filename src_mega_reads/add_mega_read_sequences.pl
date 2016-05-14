eecd0c0e6f13c34bbec71cb9e1f143d9880fa2d5
Aleksey Zimin <alekseyz@ipst.umd.edu>
2016-05-14 15:28:56 -0400

    debugged version

--- src_mega_reads/add_mega_read_sequences.pl
+++ src_mega_reads/add_mega_read_sequences.pl
@@ -5,11 +5,11 @@ my @f,$kuname, $line,$kusize=10000000,@kunitigs;
 open(FILE,$kufile);
 while($line=<FILE>){
   chomp($line);
-  if($line=/^>/){
+  if($line=~/^\>/){
     @f=split(/\s+/,$line);
     $kuname=substr($f[0],1)*2;
   }else{
-    $kusize=length($line) if($kusize<length($line));
+    $kusize=length($line) if($kusize>length($line));
     $kunitigs[$kuname]=$line;
     $line=~tr/ACGTNacgtn/TGCANtgcan/;
     $line=reverse($line);
@@ -20,7 +20,7 @@ $kusize--;#need to know k-1
 #now we read in the mega reads file and add in the sequences
 while($line=<STDIN>){
   chomp($line);
-  if($line=/^>/){ 
+  if($line=~/^\>/){ 
     print $line,"\n";
   }else{
     @f=split(/\s+/,$line);
