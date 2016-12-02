7828a11b9af5c34d2c32f111fb4cb8357154fc7b
Aleksey Zimin <alekseyz@ipst.umd.edu>
2016-12-02 15:38:40 -0500

    changed deduplicate to explain everything better, inserted trial code to eliminate singleton overlaps

--- src_mega_reads/deduplicate_unitigs.sh
+++ src_mega_reads/deduplicate_unitigs.sh
@@ -11,22 +11,31 @@ export PATH=$CA_PATH:$MYPATH:$PATH;
 
 set -e
 
+#-CS option in bogart is buggy -- I think it is still worth to place contains into singletons to turn them into regular unitigs for subsequent merging.  and then afterwards we can eliminate overlaps for those reads that end up in a unitig with only one parent
+#if [ ! -e "$ASM_DIR/eliminate_single_parent.success" ];then
+#tigStore -g $ASM_DIR/$ASM_PREFIX.gkpStore -t $ASM_DIR/$ASM_PREFIX.tigStore 2 -U -d layout | perl -ane '{if($F[0] eq "unitig"){$utg="utg".$F[1];}elsif($F[0] eq "FRG" && $F[6]==0){$maximal{$utg}++;}}END{foreach $u(keys %maximal){print $u,"\n" if($maximal{$u}==1);}}' > $ASM_DIR/duplicates.txt
+#touch "$ASM_DIR/eliminate_single_parent.success";
+#fi
+
+#here we map the unitigs against themselves to figure out which ones are redundant , and then record the reads in the redundant unitigs to eliminate their overlaps
 if [ ! -e "$ASM_DIR/self_map.success" ];then
 tigStore -g $ASM_DIR/$ASM_PREFIX.gkpStore -t $ASM_DIR/$ASM_PREFIX.tigStore 5 -U -d consensus > $ASM_DIR/unitigs.initial.fa && \
 nucmer -l 41 -c 400 -b 400 -p $ASM_DIR/asm_to_asm  $ASM_DIR/unitigs.initial.fa  $ASM_DIR/unitigs.initial.fa && \
 rm $ASM_DIR/unitigs.initial.fa && \
-awk 'BEGIN{p=1;}{if($1 ~/^>/){if(substr($1,2)==$2) p=0; else p=1;} if(p==1) print $0;}' $ASM_DIR/asm_to_asm.delta| delta-filter -q -o 20 /dev/stdin|show-coords -lcHr /dev/stdin | awk '{if($12>$13) print $0}' |merge_matches_and_tile_coords_file.pl 10000 | perl -ane '{$cov{$F[-1]}+=$F[15] if($F[15]>=10);}END{foreach $k(keys %cov){print $k,"\n" if($cov{$k}>90);}}' > $ASM_DIR/duplicates.txt && \
+awk 'BEGIN{p=1;}{if($1 ~/^>/){if(substr($1,2)==$2) p=0; else p=1;} if(p==1) print $0;}' $ASM_DIR/asm_to_asm.delta| delta-filter -q -o 20 /dev/stdin|show-coords -lcHr /dev/stdin | awk '{if($12>$13) print $0}' |merge_matches_and_tile_coords_file.pl 10000 | perl -ane '{$cov{$F[-1]}+=$F[15] if($F[15]>=10);}END{foreach $k(keys %cov){print $k,"\n" if($cov{$k}>90);}}' >> $ASM_DIR/duplicates.txt && \
 tigStore -g $ASM_DIR/$ASM_PREFIX.gkpStore -t $ASM_DIR/$ASM_PREFIX.tigStore 5 -U -d layout | awk '{if($1 ~/^unitig/){unitig=$2;}else if($1~/^FRG/){print $5" utg"unitig}}' | perl -ane 'BEGIN{open(FILE,"'$ASM_DIR/duplicates.txt'");while($l=<FILE>){chomp($l);$d{$l}=1}}{print $F[0],"\n" if(defined($d{$F[1]}));}' > $ASM_DIR/duplicates.iid.txt && \
 touch $ASM_DIR/self_map.success
 fi
 
+#here we examine 22-mers in the unique unitigs and build a database of them that have a count of 9 or above
 if [ ! -e "$ASM_DIR/unitig_mer.success" ];then
 tigStore -g $ASM_DIR/$ASM_PREFIX.gkpStore -t $ASM_DIR/$ASM_PREFIX.tigStore 5 -U -d layout |tr -d '-' | \
 awk 'BEGIN{print ">unique unitigs"}{if($1 == "cns"){seq=$2}else if($1 == "data.unitig_coverage_stat" && $2>=5){print seq"N"}}' | \
-jellyfish count -L 10 -C -m $OVL_MER -s $ESTIMATED_GENOME_SIZE -t $NUM_THREADS -o $ASM_DIR/unitig_mers /dev/fd/0 && \
+jellyfish count -L 9 -C -m $OVL_MER -s $ESTIMATED_GENOME_SIZE -t $NUM_THREADS -o $ASM_DIR/unitig_mers /dev/fd/0 && \
 touch $ASM_DIR/unitig_mer.success
 fi
 
+#we backup the assembly
 if [ -e "$ASM_DIR/self_map.success" ] && [ -e "$ASM_DIR/unitig_mer.success" ];then
 rm -rf $ASM_DIR/ovlStoreBackup
 mkdir -p $ASM_DIR/ovlStoreBackup && \
@@ -36,11 +45,13 @@ echo "Failed to self-map or build unitig mer database: no $ASM_DIR/self_map.succ
 exit
 fi
 
+#we filter the overlaps -- if ALL k-mers in the overlap region are repetitive from above -- break overlap
 if [ ! -e "$ASM_DIR/overlap_filter.success" ];then
 overlapStore -d $ASM_DIR/ovlStoreBackup/$ASM_PREFIX.ovlStore | perl -ane 'BEGIN{open(FILE,"'$ASM_DIR/duplicates.iid.txt'");while($l=<FILE>){chomp($l);$diid{$l}=1}}{if($F[0]<$F[1]){if(not(defined($diid{$F[0]})) && not(defined($diid{$F[1]}))){ print join(" ",@F[0..6]),"\n"}}}'  | filter_overlap_file -t $NUM_THREADS <(gatekeeper  -dumpfragments -withsequence $ASM_DIR/$ASM_PREFIX.gkpStore| grep -P '^fragmentIdent|^fragmentSequence' | \
 perl -ane 'BEGIN{$flag=1}{if($flag){print ">";}print "$F[2]\n";$flag=1-$flag;}') $ASM_DIR/unitig_mers /dev/fd/0 | convertOverlap -ovl | gzip -c > $ASM_DIR/overlaps_dedup.ovb.gz && touch $ASM_DIR/overlap_filter.success
 fi
 
+#rebuild the overlapstore
 if [ ! -e "$ASM_DIR/overlapStore_rebuild.success" ];then
 overlapStoreBuild -o $ASM_DIR/$ASM_PREFIX.ovlStore -M 65536 -g $ASM_DIR/$ASM_PREFIX.gkpStore $ASM_DIR/overlaps_dedup.ovb.gz 1>$ASM_DIR/overlapStore.rebuild.err 2>&1 && rm $ASM_DIR/overlaps_dedup.ovb.gz && touch $ASM_DIR/overlapStore_rebuild.success
 fi
