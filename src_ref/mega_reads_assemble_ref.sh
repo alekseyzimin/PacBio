f2ecba24ddd98f3ae2525b669950ba9ffc7d8285
Aleksey Zimin <alekseyz@ipst.umd.edu>
2017-05-16 18:03:26 -0400

    bugfix

--- src_ref/mega_reads_assemble_ref.sh
+++ src_ref/mega_reads_assemble_ref.sh
@@ -234,7 +234,7 @@ exit;
 fi
 
 #recompute astat if low pacbio coverage
-if [ $MCOVERAGE -le 5 ]; then
+if [ $COVERAGE -le 5 ]; then
 if [ ! -e ${CA}/recompute_astat.success ];then
 recompute_astat_superreads_CA8.sh genome $CA $PE_AVG_READ_LENGTH $MASURCA_ASSEMBLY_WORK1_PATH/readPlacementsInSuperReads.final.read.superRead.offset.ori.txt  $SR_FRG
 touch ${CA}/recompute_astat.success
