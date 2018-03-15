4a253d4cf2343ac93499bf2aa90a451f02eb6931
Aleksey Zimin <alekseyz@ipst.umd.edu>
2018-03-15 13:10:21 -0400

    changed ploidy estimation

--- src_mega_reads/mega_reads_assemble_cluster.sh
+++ src_mega_reads/mega_reads_assemble_cluster.sh
@@ -123,7 +123,7 @@ fi
 if [ -s PLOIDY.txt ];then
 PLOIDY=`head -n 1 PLOIDY.txt`
 else
-PLOIDY=$(($JF_SIZE/$ESTIMATED_GENOME_SIZE/2))
+PLOIDY=$(($JF_SIZE/$ESTIMATED_GENOME_SIZE/3))
 fi
 if [ $PLOIDY -lt 1 ];then PLOIDY=1; fi
 if [ $PLOIDY -gt 2 ];then PLOIDY=2; fi
