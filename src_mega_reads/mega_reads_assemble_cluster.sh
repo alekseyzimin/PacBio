75febbd4c466d0bd71384634d2c51d0a09e3d57c
Aleksey Zimin <alekseyz@ipst.umd.edu>
2018-08-22 07:48:09 -0400

    increased max_gap to 1000, gaps over that will be subject to consensus, otherwise just join  -- this works better overall on multiple data sets

--- src_mega_reads/mega_reads_assemble_cluster.sh
+++ src_mega_reads/mega_reads_assemble_cluster.sh
@@ -8,8 +8,8 @@ MYPATH="`dirname \"$0\"`"
 MYPATH="`( cd \"$MYPATH\" && pwd )`"
 ESTIMATED_GENOME_SIZE=0
 #minimum 50
-MAX_GAP=100
-#MAX_GAP=1000
+#MAX_GAP=100
+MAX_GAP=1000
 MER=15
 B=17
 d=0.029
@@ -540,8 +540,8 @@ echo "Gap consensus"
     for i in $(seq 1 $TOJOIN_BATCHES);do join_cons_names[$i]="join_consensus.$i.fasta";done;
     for i in $(seq 1 $TOJOIN_BATCHES);do merges_names[$i]="merges.$i.txt";done;
 
-    awk 'BEGIN{flag=1}{if($2>int("'$MAX_GAP'")/2 && $6==1) {if($3==prev3 && $4==prev4) flag++; else flag=1;  print flag" "$1" "$3" "$4;prev3=$3;prev4=$4}}'  ../${COORDS}.1.allowed |grep '^1 ' |awk '{print $0}' > refs.txt && \
-    awk 'BEGIN{flag=1}{if($2>int("'$MAX_GAP'")/2 && $6==1) {if($3==prev3 && $4==prev4) flag++; else flag=1;  print flag" "$1" "$3" "$4;prev3=$3;prev4=$4}}'  ../${COORDS}.1.allowed |awk '{print $0}' > qrys.txt && \
+    awk 'BEGIN{flag=1}{if($2>int("'$MAX_GAP'")*.75 && $6==1) {if($3==prev3 && $4==prev4) flag++; else flag=1;  print flag" "$1" "$3" "$4;prev3=$3;prev4=$4}}'  ../${COORDS}.1.allowed |grep '^1 ' |awk '{print $0}' > refs.txt && \
+    awk 'BEGIN{flag=1}{if($2>int("'$MAX_GAP'")*.75 && $6==1) {if($3==prev3 && $4==prev4) flag++; else flag=1;  print flag" "$1" "$3" "$4;prev3=$3;prev4=$4}}'  ../${COORDS}.1.allowed |awk '{print $0}' > qrys.txt && \
     if [ ! -s qrys.all.fa ]; then ufasta extract -f <(awk '{print $2}' qrys.txt) ../$LONGREADS1 > qrys.all.fa.tmp && mv qrys.all.fa.tmp qrys.all.fa; fi && \
     ufasta extract -v -f <(awk '{print $2}' refs.txt) qrys.all.fa > qrys.fa && \
     ufasta extract -f <(awk '{print $2}' refs.txt) qrys.all.fa > refs.fa && \
