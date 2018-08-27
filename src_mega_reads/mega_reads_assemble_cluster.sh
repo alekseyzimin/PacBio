9cc868d1842ec92fb71d0fc1e70f7af16444023b
Aleksey Zimin <alekseyz@ipst.umd.edu>
2018-08-27 16:01:35 -0400

    code/error message cleanup

--- src_mega_reads/mega_reads_assemble_cluster.sh
+++ src_mega_reads/mega_reads_assemble_cluster.sh
@@ -25,6 +25,12 @@ PACBIO=""
 NANOPORE=""
 ONEPASS=0
 
+log () {
+  d=$(date)
+    echo -e "${GC}[$d]${NC} $@"
+}
+
+
 function error_exit {
     echo "$1" >&2   
     exit "${2:-1}" 
@@ -112,10 +118,9 @@ do
 done
 
 ###############checking arguments#########################
-echo "Using CABOG from is $CA_PATH"
+log "Using CABOG from is $CA_PATH"
 if [ ! -e $CA_PATH/runCA ];then
-    echo "runCA not found at $CA_PATH!";
-    exit 1;
+    error_exit "runCA not found at $CA_PATH!";
 fi
 
 export PATH=$MYPATH:$CA_PATH:$PATH
@@ -123,8 +128,7 @@ export PATH=$MYPATH:$CA_PATH:$PATH
 ################setting parameters#########################
 JF_SIZE=$(stat -L -c%s $MASURCA_ASSEMBLY_WORK1_PATH/superReadSequences.fasta);
 if [ $ESTIMATED_GENOME_SIZE -lt 1 ];then 
-    echo "Estimated Genome Size is invalid or missing";
-    exit;
+    error_exit "Estimated Genome Size is invalid or missing";
 fi
 if [ -s PLOIDY.txt ];then
 PLOIDY=`head -n 1 PLOIDY.txt`
@@ -138,12 +142,12 @@ CA=CA.${COORDS}
 echo $CA > CA_dir.txt
 echo $PLOIDY > PLOIDY.txt
 
-echo "Running mega-reads correction/assembly"
-echo "Using mer size $MER for mapping, B=$B, d=$d"
-echo "Estimated Genome Size $ESTIMATED_GENOME_SIZE"
-echo "Estimated Ploidy $PLOIDY"
-echo "Using $NUM_THREADS threads"
-echo "Output prefix $COORDS"
+log "Running mega-reads correction/assembly"
+log "Using mer size $MER for mapping, B=$B, d=$d"
+log "Estimated Genome Size $ESTIMATED_GENOME_SIZE"
+log "Estimated Ploidy $PLOIDY"
+log "Using $NUM_THREADS threads"
+log "Output prefix $COORDS"
 
 rm -f .rerun
 ###############removing redundant subreads or reducing the coverage by picking the longest reads##############################
@@ -166,7 +170,7 @@ PB_SIZE=$(stat -L -c%s $LONGREADS);
 FIRSTCHAR=`zcat -f $LONGREADS | head -c 1`;
 if [ "$LONGREADS" = "$NANOPORE" ];then
     if [ ! -s "ont_${PB_HC}xlongest.fa" ] ;then
-	echo "Using ${PB_HC}x of the longest ONT reads" 
+	log "Using ${PB_HC}x of the longest ONT reads" 
 	if [ "$FIRSTCHAR" = ">" ];then
           zcat -f $LONGREADS |ufasta extract -f <(zcat -f $LONGREADS | ufasta sizes -H | LC_ALL=C sort -nrk2 -S50% | perl -ane 'BEGIN{$thresh=int("'$ESTIMATED_GENOME_SIZE'")*int("'${PB_HC}'")*int("'$PLOIDY'");$n=0}{$n+=$F[1];print $F[0],"\n" if($n<$thresh)}') /dev/stdin | ufasta one > ont_${PB_HC}xlongest.fa.tmp && mv ont_${PB_HC}xlongest.fa.tmp ont_${PB_HC}xlongest.fa || error_exit "failed to extract the best long reads";
         else
@@ -180,7 +184,7 @@ if [ "$LONGREADS" = "$NANOPORE" ];then
     LONGREADS1="ont_${PB_HC}xlongest.fa";
 else
     if [ $(($PB_SIZE/$ESTIMATED_GENOME_SIZE/$PLOIDY)) -gt ${PB_HC} ];then
-	echo "Pacbio coverage >${PB_HC}x, using ${PB_HC}x of the longest reads";
+	log "Pacbio coverage >${PB_HC}x, using ${PB_HC}x of the longest reads";
 	if [ ! -s "pacbio_${PB_HC}xlongest.fa" ] ;then
 	    if [ "$FIRSTCHAR" = ">" ];then
 		zcat -f $LONGREADS |ufasta extract -f <(zcat -f $LONGREADS | grep --text '^>' | awk '{split($1,a,"/");split(a[3],b,"_");len=b[2]-b[1];if($2 ~ /^RQ/){split($2,c,"=");len=int(len*c[2]/0.85);}print substr($1,2)" "len;}'  | LC_ALL=C sort -nrk2 -S50% | perl -ane 'BEGIN{$thresh=int("'$ESTIMATED_GENOME_SIZE'")*int("'${PB_HC}'")*int("'$PLOIDY'");$n=0}{$n+=$F[1];print $F[0],"\n" if($n<$thresh)}') /dev/stdin |ufasta one > pacbio_${PB_HC}xlongest.fa.tmp && mv pacbio_${PB_HC}xlongest.fa.tmp pacbio_${PB_HC}xlongest.fa || error_exit "failed to extract the best long reads";
@@ -194,7 +198,7 @@ else
 	fi
 	LONGREADS1="pacbio_${PB_HC}xlongest.fa";
     else
-	echo "Pacbio coverage <${PB_HC}x, using the longest subreads";
+	log "Pacbio coverage <${PB_HC}x, using the longest subreads";
 	if [ ! -s "pacbio_nonredundant.fa" ] ;then
 	    if [ "$FIRSTCHAR" = ">" ];then
 		zcat -f $LONGREADS |ufasta extract -f <(zcat -f $LONGREADS |grep --text '^>' | awk '{print $1}' | awk -F '/' '{split($3,a,"_");print substr($0,2)" "$1"/"$2" "a[2]-a[1]}' | LC_ALL=C sort -nrk3 -S50% | perl -ane '{if(not(defined($h{$F[1]}))){$h{$F[1]}=1;print $F[0],"\n"}}') /dev/stdin |ufasta one > pacbio_nonredundant.fa.tmp && mv pacbio_nonredundant.fa.tmp pacbio_nonredundant.fa || error_exit "failed to extract the longest subreads"; 
@@ -213,7 +217,7 @@ fi
 
 #first we re-create k-unitigs and super reads with smaller K
 if [ ! -e superReadSequences.named.fasta ];then
-    echo "Reducing super-read k-mer size"
+    log "Reducing super-read k-mer size"
     awk 'BEGIN{n=0}{if($1~/^>/){}else{print ">sr"n"\n"$0;n+=2;}}' $MASURCA_ASSEMBLY_WORK1_PATH/superReadSequences.fasta > superReadSequences.fasta.in.tmp && mv  superReadSequences.fasta.in.tmp  superReadSequences.fasta.in || error_exit "failed to create superReadSequences.fasta.in"
     create_k_unitigs_large_k -q 1 -c $(($KMER-1)) -t $NUM_THREADS -m $KMER -n $(($ESTIMATED_GENOME_SIZE*2)) -l $KMER -f `perl -e 'print 1/'$KMER'/1e5'` $MASURCA_ASSEMBLY_WORK1_PATH/superReadSequences.fasta.all  | grep --text -v '^>' | perl -ane '{$seq=$F[0]; $F[0]=~tr/ACTGactg/TGACtgac/;$revseq=reverse($F[0]); $h{($seq ge $revseq)?$seq:$revseq}=1;}END{$n=0;foreach $k(keys %h){print ">",$n++," length:",length($k),"\n$k\n"}}' > guillaumeKUnitigsAtLeast32bases_all.fasta.tmp && mv guillaumeKUnitigsAtLeast32bases_all.fasta.tmp guillaumeKUnitigsAtLeast32bases_all.$KMER.fasta || error_exit "failed to create k-unitigs for small k super reads";
     rm -rf work1_mr
@@ -238,20 +242,17 @@ fi
 
 SUPERREADS=superReadSequences.named.fasta
 if [ ! -s superReadSequences.named.fasta ];then
-    echo "Error creating named super-reads file ";
-    exit 1;
+    error_exit "Error creating named super-reads file ";
 fi
 
 KUNITIGS=guillaumeKUnitigsAtLeast32bases_all.$KMER.fasta
 if [ ! -s $KUNITIGS ];then
-    echo "K-unitigs file $KUNITIGS not found!";
-    exit 1;
+    error_exit "K-unitigs file $KUNITIGS not found!";
 fi
 
 KUNITIGLENGTHS=work1_mr/kUnitigLengths.txt
 if [ ! -s $KUNITIGLENGTHS ];then
-    echo "K-unitig lengths file $KUNITIGLENGTHS not found!";
-    exit 1;
+    error_exit "K-unitig lengths file $KUNITIGLENGTHS not found!";
 fi
 
 PBATCHES=$(($(stat -c%s -L $LONGREADS1)/$PBATCH_SIZE));
@@ -267,10 +268,10 @@ for i in $(seq 1 $PBATCHES);do larr[$i]="lr.batch$i";done;
 for i in $(seq 1 $PBATCHES);do lmrOut[$i]="mr.batch$i.txt";done;
 
 if [ ! -s $COORDS.txt ] || [ -e .rerun ];then
-    echo "Mega-reads pass 1"
+    log "Mega-reads pass 1"
 
     if [ $PBATCHES -ge 2 ] && [ $USE_SGE -eq 1 ];then
-	echo "Running on the grid in $PBATCHES batches";
+	log "Running on the grid in $PBATCHES batches";
 	if [ "$QUEUE" = "" ];then
 	    error_exit "Queue for SGE is undefined, must specify which queue to submit jobs to"
 	fi
@@ -306,7 +307,7 @@ if [ ! -s $COORDS.txt ] || [ -e .rerun ];then
             done
             if [ ${#failArr[@]} -ge 1 ];then
               if [ $GRID_ENGINE = "SGE" ];then
-              echo "submitting SGE create_mega_reads jobs to the grid"
+              log "submitting SGE create_mega_reads jobs to the grid"
               qsub -q $QUEUE -cwd -j y -sync y -N "create_mega_reads"  -t 1-$PBATCHES create_mega_reads.sh 1> mqsub2.out 2>&1 || error_exit "create_mega_reads failed on the grid"
               else
               error_exit "submitting SLURM jobs to the grid.  The script will exit now.  Please re-run assemble.sh when all jobs finish."
@@ -327,7 +328,7 @@ if [ ! -s $COORDS.txt ] || [ -e .rerun ];then
 #cat the results
 	    cat ${lmrOut[@]} > ../$COORDS.tmp.txt && mv ../$COORDS.tmp.txt ../$COORDS.txt || error_exit "concatenation of mega-read grid output files failed" ) && rm -rf mr_pass1 || error_exit "mega-reads pass 1 on the grid failed or stopped, please re-run assemble.sh"
     else #single computer
-	echo "Running locally in 1 batch";
+	log "Running locally in 1 batch";
 	if numactl --show 1> /dev/null 2>&1;then
 	    numactl --interleave=all create_mega_reads -s $JF_SIZE -m $MER --psa-min 13  --stretch-cap 10000 -k $KMER -u $KUNITIGS -t $NUM_THREADS -B $B --max-count 5000 -d $d  -r $SUPERREADS  -p $LONGREADS1 -o $COORDS.txt.tmp && mv $COORDS.txt.tmp $COORDS.txt || error_exit "mega-reads pass 1 failed";
 	else
@@ -397,10 +398,10 @@ for i in $(seq 1 $SBATCHES);do arrOut[$i]="coords.batch$i.gz";done;
 for i in $(seq 1 $SBATCHES);do mrOut[$i]="mr.batch$i.txt";done;
 
 if [ ! -s $COORDS.mr.txt ] || [ -e .rerun ];then
-    echo "Mega-reads pass 2"
+    log "Mega-reads pass 2"
 
     if [ $SBATCHES -ge 2 ] && [ $USE_SGE -eq 1 ];then
-      echo "Running on the grid in $SBATCHES batches";
+      log "Running on the grid in $SBATCHES batches";
       if [ "$QUEUE" = "" ];then
           error_exit "Queue for SGE is undefined, must specify which queue to submit jobs to"
       fi
@@ -435,7 +436,7 @@ if [ ! -s $COORDS.mr.txt ] || [ -e .rerun ];then
             done
             if [ ${#failArr[@]} -ge 1 ];then
               if [ $GRID_ENGINE = "SGE" ];then
-              echo "submitting SGE jf_aligner jobs to the grid"
+              log "submitting SGE jf_aligner jobs to the grid"
               qsub -q $QUEUE -cwd -j y -sync y -N "jf_aligner"  -t 1-$SBATCHES jf_aligner.sh 1> jqsub2.out 2>&1 || error_exit "jf_aligner failed on the grid"
               else
               error_exit "submitting SLURM jobs to the grid.  The script will exit now.  Please re-run assemble.sh when all jobs finish."
@@ -461,7 +462,7 @@ if [ ! -s $COORDS.mr.txt ] || [ -e .rerun ];then
             chmod 0755 ./longest_path.sh && ./longest_path.sh 
             ) && rm -rf mr_pass2 || error_exit "mega-reads pass 2 on the grid failed or stopped, please re-run assemble.sh"
     else #single computer
-        echo "Running locally in 1 batch";
+        log "Running locally in 1 batch";
 	if numactl --show 1> /dev/null 2>&1;then
 	    numactl --interleave=all create_mega_reads --stretch-cap 6000 -s $JF_SIZE --psa-min 13 -m $(($MER+2)) -k $KMER -u $KUNITIGS -t $NUM_THREADS -B $(($B-4)) --max-count 2000 -d $d  -r $COORDS.all_mr.maximal.fa  -p <(ufasta extract -v -f $COORDS.single.txt $LONGREADS1) -o $COORDS.mr.txt.tmp && mv $COORDS.mr.txt.tmp $COORDS.mr.txt || error_exit "mega-reads pass 2 failed";
 	else
@@ -484,7 +485,7 @@ grep --text '^>' $COORDS.txt | awk  '{print substr($1,2)}' > $COORDS.single.txt
 fi
 
 if [ ! -s $COORDS.all.txt ] || [ -e .rerun ];then
-    echo "Refining alignments"
+    log "Refining alignments"
     NUM_LONGREADS_READS_PER_BATCH=`grep --text '^>'  $LONGREADS1 | wc -l | awk '{bs=int($1/1024);if(bs<1000){bs=1000};if(bs>100000){bs=100000};}END{print bs}'` 
     cat <(ufasta extract -f $COORDS.single.txt $COORDS.txt) <(ufasta extract -v -f $COORDS.single.txt $COORDS.mr.txt)| awk '{if($0~/^>/){pb=substr($1,2);print $0} else { print $3" "$4" "$5" "$6" "$10" "pb" "$11" "$9}}' | add_pb_seq.pl $LONGREADS1 | split_matches_file.pl $NUM_LONGREADS_READS_PER_BATCH .matches && ls .matches.* | xargs -P $NUM_THREADS -I % refine.sh $COORDS % $KMER && cat $COORDS.matches*.all.txt.tmp > $COORDS.all.txt && rm .matches.* && rm $COORDS.matches*.all.txt.tmp 
     touch .rerun
@@ -492,7 +493,7 @@ fi
 
 #if [ ! -s $COORDS.1.unjoined.fa ] || [ -e .rerun ];then
 if [ ! -s $COORDS.1.fa ] || [ -e .rerun ];then
-echo "Joining"
+log "Joining"
     awk 'BEGIN{flag=0}{
         if($0 ~ /^>/){
                 flag=0;
@@ -523,7 +524,7 @@ fi
 
 if [ ! -s $COORDS.1.fa ] || [ -e .rerun ];then
 #if [ ! -s $COORDS.1.fa ];then
-echo "Gap consensus"    
+log "Gap consensus"    
     #making consensus for large gaps
     mkdir -p ${COORDS}.join_consensus.tmp && \
     (cd ${COORDS}.join_consensus.tmp;
@@ -591,7 +592,7 @@ echo "Gap consensus"
     if [ -e ${COORDS}.join_consensus.tmp/join_consensus.success ];then
       cat $COORDS.1.joined.fa $COORDS.1.unjoined.fa  > $COORDS.1.fa.tmp && mv $COORDS.1.fa.tmp $COORDS.1.fa && rm -rf ${COORDS}.join_consensus.tmp $COORDS.1.to_join.fa.tmp
     else
-      echo "Warning! Some or all gap consensus jobs failed, see files in ${COORDS}.join_consensus.tmp, proceeding anyway, to rerun gap consensus erase $COORDS.1.fa and re-run assemble.sh"
+      log "Warning! Some or all gap consensus jobs failed, see files in ${COORDS}.join_consensus.tmp, proceeding anyway, to rerun gap consensus erase $COORDS.1.fa and re-run assemble.sh"
       if [ -s ${COORDS}.1.joined.fa ];then
         cat $COORDS.1.joined.fa $COORDS.1.unjoined.fa  > $COORDS.1.fa.tmp && mv $COORDS.1.fa.tmp $COORDS.1.fa
       else
@@ -605,7 +606,7 @@ echo "Gap consensus"
 fi
 
 if [ ! -s $COORDS.1.frg ] || [ ! -s $COORDS.1.mates.frg ] || [ -e .rerun ];then
-    echo "Generating assembly input files"
+    log "Generating assembly input files"
     awk 'BEGIN{n=0}{if($1~/^>/){}else{print ">sr"n"\n"$0;n+=2;}}'  $COORDS.1.fa  > mr.fa.in && \
 	create_k_unitigs_large_k -q 1 -c 30 -t $NUM_THREADS -m 31 -n $ESTIMATED_GENOME_SIZE -l 31 -n $(($ESTIMATED_GENOME_SIZE*2)) -f `perl -e 'print 1/31/1e5'` mr.fa.in   | grep --text -v '^>' | perl -ane '{$seq=$F[0]; $F[0]=~tr/ACTGactg/TGACtgac/;$revseq=reverse($F[0]); $h{($seq ge $revseq)?$seq:$revseq}=1;}END{$n=0;foreach $k(keys %h){print ">",$n++," length:",length($k),"\n$k\n"}}' > guillaumeKUnitigsAtLeast32bases_all.fasta.tmp && mv guillaumeKUnitigsAtLeast32bases_all.fasta.tmp guillaumeKUnitigsAtLeast32bases_all.31.fasta && \
 	rm -rf work1_mr1 && \
@@ -625,7 +626,7 @@ if [ $ESTIMATED_GENOME_SIZE -gt 1 ];then
     MR_SIZE=$(stat -c%s -L "$COORDS.1.fa");
     MCOVERAGE=$(($MR_SIZE/$ESTIMATED_GENOME_SIZE/$PLOIDY+1));
     if [ $MCOVERAGE -le 5 ];then
-	echo "Coverage of the mega-reads less than 5 -- using the super reads as well";
+	log "Coverage of the mega-reads less than 5 -- using the super reads as well";
 	SR_FRG=$COORDS.sr.frg
 	if [ ! -s $SR_FRG ];then
 	    awk '{if($0 ~ /^>/) print $0":super-read"; else print $0}' $MASURCA_ASSEMBLY_WORK1_PATH/superReadSequences.fasta | fasta2frg.pl sr 200 > $SR_FRG.tmp && mv  $SR_FRG.tmp  $SR_FRG || error_exit "failed to create super-reads frg file";
@@ -642,7 +643,7 @@ OVLMIN=`head -n 100000 $SR_FRG $COORDS.1.frg $OTHER_FRG 2>/dev/null | grep -A 1 
 batOptions="-repeatdetect $TCOVERAGE $TCOVERAGE $TCOVERAGE -el $OVLMIN -RS"
 OVL_MER=22
 
-echo "Coverage threshold for splitting unitigs is $TCOVERAGE minimum ovl $OVLMIN"
+log "Coverage threshold for splitting unitigs is $TCOVERAGE minimum ovl $OVLMIN"
 let NUM_THREADSd4=$(($NUM_THREADS/4+1))
 if [ $USE_SGE -ge 1 ];then
 OVL_THREADS=4
@@ -691,7 +692,7 @@ cgwDemoteRBP=0
 cnsReuseUnitigs=1" > runCA.spec
 
 
-echo "Running assembly"
+log "Running assembly"
 if [ ! -e "${CA}/5-consensus/consensus.success" ]; then 
   #need to start from the beginning
   #this is helpful for re-starts
@@ -709,8 +710,7 @@ fi
 
 #at athis point we check if the unitig consensus is done
 if [ ! -e "${CA}/5-consensus/consensus.success" ]; then
-    echo "Assembly stopped or failed, see $CA.log" 
-    exit;
+    error_exit "Assembly stopped or failed, see $CA.log" 
 fi
 
 if [ ! -e "${CA}/deduplicate.success" ]; then
@@ -726,14 +726,13 @@ if [ ! -e "${CA}/5-consensus/consensus.success" ]; then
 fi
 
 if [ ! -e "${CA}/5-consensus/consensus.success" ]; then
-    echo "Assembly stopped or failed, see $CA.log"
-    exit;
+    error_exit "Assembly stopped or failed, see $CA.log"
 fi
 
 #recompute astat if low pacbio coverage
 if [ $MCOVERAGE -le 5 ]; then
     if [ ! -e ${CA}/recompute_astat.success ];then
-	echo "Recomputing A-stat"
+	log "Recomputing A-stat"
 	recompute_astat_superreads_CA8.sh genome $CA $PE_AVG_READ_LENGTH $MASURCA_ASSEMBLY_WORK1_PATH/readPlacementsInSuperReads.final.read.superRead.offset.ori.txt  $SR_FRG
 	touch ${CA}/recompute_astat.success
     fi
@@ -743,10 +742,10 @@ fi
 $CA_PATH/runCA -s runCA.spec consensus=pbutgcns -p genome -d $CA  stopAfter=consensusAfterScaffolder $COORDS.1.frg $COORDS.1.mates.frg $SR_FRG $OTHER_FRG 1>> $CA.log 2>&1
 rm -rf $CA/8-consensus/*.success $CA/8-consensus/consensus.sh
 $CA_PATH/runCA -s runCA.spec -p genome -d $CA  cnsConcurrency=$(($NUM_THREADS/2+1)) $COORDS.1.frg $COORDS.1.mates.frg  $SR_FRG $OTHER_FRG 1>> $CA.log 2>&1 && \
-echo "Mega-reads initial assembly complete" || echo "Assembly stopped or failed, see $CA.log"
+log "Mega-reads initial assembly complete" || error_exit "Assembly stopped or failed, see $CA.log"
 
 if [ ! -e "${CA}/10-gapclose/gapclose.success" ] && [ $(stat -c%s ${CA}/9-terminator/genome.scf.fasta) -gt $(stat -c%s ${CA}/9-terminator/genome.ctg.fasta) ] ; then
-  echo "Closing gaps in scaffolds"
+  log "Closing gaps in scaffolds"
   mkdir -p ${CA}/10-gapclose
   (cd ${CA}/10-gapclose && \
   process_scaffold_gaps.pl ../9-terminator/genome.posmap.ctgscf ../9-terminator/genome.posmap.frgctg |sort -k 2 -S 10% > read_scaffold.txt && \
