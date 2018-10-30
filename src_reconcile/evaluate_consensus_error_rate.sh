752fbd7681903640d3b6f80721abbed0297e79c8
Aleksey Zimin <alekseyz@ipst.umd.edu>
2018-10-30 11:35:13 -0400

    added evaluate_consensus_error_rate.sh script

---
+++ src_reconcile/evaluate_consensus_error_rate.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+
+set -o pipefail
+set -e
+NUM_THREADS=4
+
+#parsing arguments
+while [[ $# > 0 ]]
+do
+    key="$1"
+
+    case $key in
+        -t|--threads)
+            NUM_THREADS="$2"
+            shift
+            ;;
+        -a|--assembly)
+            ASM="$2"
+            shift
+            ;;
+        -r|--reads)
+            READS="$2";
+            shift
+            ;;
+        -v|--verbose)
+            set -x
+            ;;
+        -h|--help|-u|--usage)
+            echo "Usage:  evaluate_consensus_error_rate.sh -a <assembly contigs or scaffolds> -r <Illumina reads fastq> -t <number of threads>"
+            echo "Must have bwa, samtools and freebayes available on the PATH"
+            exit 0
+            ;;
+        *)
+            echo "Unknown option $1"
+            exit 1        # unknown option
+            ;;
+    esac
+    shift
+done
+
+if [ ! -e $ASM ] || [ ! -e $READS ];then
+echo "assembly file $ASM or Illumina reads file $READS not found!"
+exit 1
+fi
+
+BASM=`basename $ASM`
+BWA=`which bwa`
+FREEBAYES=`which freebayes`
+SAMTOOLS=`which samtools`
+
+if [ ! -e $BASM.index.success ];then 
+rm -f $BASM.map.success
+$BWA index $ASM -p $BASM.bwa && touch $BASM.index.success
+fi
+
+if [ ! -e $BASM.map.success ];then
+rm -f $BASM.sort.success
+$BWA mem -p -t 64 $BASM.bwa $READS 2>bwasterr |samtools view -bhS /dev/stdin > $BASM.unSorted.bam && touch $BASM.map.success
+fi
+
+if [ ! -e $BASM.sort.success ];then
+rm -f $BASM.vc.success
+$SAMTOOLS sort -m64000000000  $BASM.unSorted.bam $BASM.alignSorted && \
+$SAMTOOLS index $BASM.alignSorted.bam && touch  $BASM.sort.success
+fi
+
+if [ ! -e $BASM.vc.success ];then
+rm -f $BASM.calc.success
+freebayes -C 2 -0 -O -q 20 -z 0.02 -E 0 -X -u -p 1 -F 0.5 -b $BASM.alignSorted.bam  -v $BASM.vcf -f $ASM && touch $BASM.vc.success
+fi
+
+NUMERR=`grep -v "#" $BASM.vcf  |perl -ane '{if(length($F[3])==1 && length($F[3])==1){$nerr=1;}else{$nerr=abs(length($F[2])-length($F[3]));}print "$F[9]:$nerr\n";}' | awk -F ':' '{if($6>=3 && $4==0) nerr+=$NF}END{print nerr}'`
+ASMSIZE=`ufasta n50 -S $ASM | awk '{print $2}'`
+QUAL=`echo $NUMERR $ASMSIZE | awk '{print 100-$1/$2*100}'`
+echo "Errors: $NUMERR"
+echo "AsmSize: $ASMSIZE"
+echo "ConsensusQuality: $QUAL"
+
