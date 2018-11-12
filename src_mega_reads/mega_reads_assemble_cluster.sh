c50ad9d6af7495c198e0d2b8d2911d012496c22c
Aleksey Zimin <alekseyz@ipst.umd.edu>
2018-11-12 17:00:09 -0500

    we do not need to recompute k-unitig lengths

--- src_mega_reads/mega_reads_assemble_cluster.sh
+++ src_mega_reads/mega_reads_assemble_cluster.sh
@@ -629,7 +629,7 @@ if [ ! -s $COORDS.1.frg ] || [ ! -s $COORDS.1.mates.frg ] || [ -e .rerun ];then
 	create_k_unitigs_large_k -q 1 -c 30 -t $NUM_THREADS -m 31 -n $ESTIMATED_GENOME_SIZE -l 31 -n $(($ESTIMATED_GENOME_SIZE*2)) -f `perl -e 'print 1/31/1e5'` mr.fa.in   | grep --text -v '^>' | perl -ane '{$seq=$F[0]; $F[0]=~tr/ACTGactg/TGACtgac/;$revseq=reverse($F[0]); $h{($seq ge $revseq)?$seq:$revseq}=1;}END{$n=0;foreach $k(keys %h){print ">",$n++," length:",length($k),"\n$k\n"}}' > guillaumeKUnitigsAtLeast32bases_all.fasta.tmp && mv guillaumeKUnitigsAtLeast32bases_all.fasta.tmp guillaumeKUnitigsAtLeast32bases_all.31.fasta && \
 	rm -rf work1_mr1 && \
 	createSuperReadsForDirectory.perl --stopAfter joinKUnitigs -minreadsinsuperread 1 -l 31 -mean-and-stdev-by-prefix-file meanAndStdevByPrefix.pe.txt -kunitigsfile guillaumeKUnitigsAtLeast32bases_all.31.fasta -t $NUM_THREADS -mikedebug work1_mr1 mr.fa.in  1> super1.err 2>&1 && \
-        cat work1_mr1/readPositionsInSuperReads |get_super_read_sizes.pl <(ufasta sizes -H  guillaumeKUnitigsAtLeast32bases_all.31.fasta) | sort -S 90% -nrk2 |uniq > work1_mr1/sr_sizes.tmp && \
+        cat work1_mr1/readPositionsInSuperReads |get_super_read_sizes.pl work1_mr1/kUnitigLengths.txt | sort -S 90% -nrk2 |uniq > work1_mr1/sr_sizes.tmp && \
         reduce_sr `cat work1_mr1/numKUnitigs.txt` work1_mr1/kUnitigLengths.txt 31 work1_mr1/sr_sizes.tmp -o work1_mr1/reduce.tmp 1>reduce2.out 2>&1 && \
         translate_reduced_reads.pl work1_mr1/reduce.tmp < work1_mr1/readPositionsInSuperReads > work1_mr1/readPlacementsInSuperReads.final.read.superRead.offset.ori.txt && \
 	find_contained_reads.pl work1_mr1/readPlacementsInSuperReads.final.read.superRead.offset.ori.txt $COORDS.1.fa > containees.txt && \
