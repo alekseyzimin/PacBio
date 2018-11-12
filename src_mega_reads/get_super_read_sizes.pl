8751ee7723ac775b09391f83e500f568c0dc5873
Aleksey Zimin <alekseyz@ipst.umd.edu>
2018-11-12 17:04:55 -0500

    changed hash to array in loading k-unitig sizes

--- src_mega_reads/get_super_read_sizes.pl
+++ src_mega_reads/get_super_read_sizes.pl
@@ -5,9 +5,9 @@ my $line,$minkusize=10000000;
 open(FILE,$kuSizeFile); #output of ufasta sizes -H on kunitigs file
 while($line=<FILE>){
   chomp($line);
-  my @f=split(' ',$line);
-  $kusize{$f[0]}=$f[1];
-  $minkusize=$f[1] if($f[1]< $minkusize);
+  my ($ku,$sz)=split(/\s+/,$line);
+  $kusize[$ku]=$sz;
+  $minkusize=$sz if($sz< $minkusize);
   }
 $minkusize--;#need to know k-1
 #now we read in the read positions file and compute the super-read sizes
@@ -15,10 +15,10 @@ while($line=<STDIN>){
   chomp($line);
   my @f=split(' ',$line);
   @srname=split("_",$f[1]);
-  my $srsize=$kusize{substr($srname[0],0,-1)};
+  my $srsize=$kusize[substr($srname[0],0,-1)];
   
   for(my $i=1;$i<=$#srname; $i++){
-    $srsize+=($kusize{substr($srname[$i],0,-1)}-$minkusize);
+    $srsize+=($kusize[substr($srname[$i],0,-1)]-$minkusize);
     }
     print "$f[1] $srsize\n";
 }
