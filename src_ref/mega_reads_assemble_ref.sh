1a8232d386f6ee8ab51c5d5cd11da7a96223b91b
Aleksey Zimin <alekseyz@ipst.umd.edu>
2019-03-01 09:58:02 -0500

    updates to reference assembly pipeline

--- src_ref/mega_reads_assemble_ref.sh
+++ src_ref/mega_reads_assemble_ref.sh
@@ -12,6 +12,20 @@ MER=17
 B=20
 d=0.05
 KMER=41
+GC=
+RC=
+NC=
+if tty -s < /dev/fd/1 2> /dev/null; then
+GC='\e[0;32m'
+RC='\e[0;31m'
+NC='\e[0m'
+fi
+
+log () {
+  dddd=$(date)
+  echo -e "${GC}[$dddd]${NC} $@"
+}
+
 
 
 function error_exit {
@@ -86,26 +100,24 @@ fi
 export PATH=$CA_PATH:$MYPATH:$PATH
 
 if [ ! -e $REF ];then
-echo "Reference reads file $REF not found!";
+echo "Reference sequence file $REF not found!";
 exit 1 ;
 fi
 
 COORDS=mr.$KMER.$MER.$B.$d
 CA=CA.${COORDS}
 
-
-echo "Running mega-reads correction/assembly"
-echo "Using mer size $MER for mapping, B=$B, d=$d"
-echo "Using MaSuRCA files from $MASURCA_ASSEMBLY_WORK1_PATH, k-unitig mer $KMER"
-echo "Using CA installation from $CA_PATH"
-echo "Using $NUM_THREADS threads"
-echo "Output prefix $COORDS"
+log "Running synteny-guided assembly"
+log "Using mer size $MER for mapping, B=$B, d=$d"
+log "Using CA installation from $CA_PATH"
+log "Using $NUM_THREADS threads"
+log "Output prefix $COORDS"
 
 rm -f .rerun
 
 #first we re-create k-unitigs and super reads with smaller K
 if [ ! -e superReadSequences.named.fasta ];then
-echo "Reducing super-read k-mer size"
+log "Reducing super-read k-mer size"
 awk 'BEGIN{n=0}{if($1~/^>/){}else{print ">sr"n"\n"$0;n+=2;}}' $MASURCA_ASSEMBLY_WORK1_PATH/superReadSequences.fasta > superReadSequences.fasta.in
 create_k_unitigs_large_k -q 1 -c $(($KMER-1)) -t $NUM_THREADS -m $KMER -n $(($ESTIMATED_GENOME_SIZE*2)) -l $KMER -f `perl -e 'print 1/'$KMER'/1e5'` $MASURCA_ASSEMBLY_WORK1_PATH/superReadSequences.fasta.all  | grep --text -v '^>' | perl -ane '{$seq=$F[0]; $F[0]=~tr/ACTGactg/TGACtgac/;$revseq=reverse($F[0]); $h{($seq ge $revseq)?$seq:$revseq}=1;}END{$n=0;foreach $k(keys %h){print ">",$n++," length:",length($k),"\n$k\n"}}' > guillaumeKUnitigsAtLeast32bases_all.fasta.tmp && mv guillaumeKUnitigsAtLeast32bases_all.fasta.tmp guillaumeKUnitigsAtLeast32bases_all.$KMER.fasta
 rm -rf work1_mr
@@ -123,23 +135,22 @@ END{
   }
 }' < work1_mr/superReadNames.txt > superReadSequences.named.fasta.tmp && mv superReadSequences.named.fasta.tmp superReadSequences.named.fasta
 rm -f superReadSequences.fasta.in
+touch .rerun
 fi
 
 SUPERREADS=superReadSequences.named.fasta
 if [ ! -s superReadSequences.named.fasta ];then
-echo "Error creating named super-reads file ";
-exit 1;
+error_exit "Error creating named super-reads file ";
 fi
 
 KUNITIGS=guillaumeKUnitigsAtLeast32bases_all.$KMER.fasta
 if [ ! -s $KUNITIGS ];then
-echo "K-unitigs file $KUNITIGS not found!";
-exit 1;
+error_exit "K-unitigs file $KUNITIGS not found!";
 fi
 
 REF_SPLIT="reference.split.fa"
 if [ ! -s $REF_SPLIT ] || [ -e .rerun ];then
-echo "Preparing reference"
+log "Preparing reference"
 perl -ane '{
   if($F[0]=~/^>/){
     if(length($seq)>0){
@@ -183,12 +194,13 @@ perl -ane '{
     $n+=length($c);
   }
 }'  $REF > $REF_SPLIT.tmp && mv $REF_SPLIT.tmp $REF_SPLIT
+touch .rerun
 fi
 
 JF_SIZE=$(stat -c%s $KUNITIGS);
 
 if [ ! -s $COORDS.txt ] || [ -e .rerun ];then
-echo "Mega-reads pass 1"
+log "Mega-reads pass 1"
 if numactl --show 1> /dev/null 2>&1;then
 numactl --interleave=all create_mega_reads -s $JF_SIZE -O 1.1 -e 5 -m $MER --psa-min 13  --stretch-cap 10000 -k $KMER -u $KUNITIGS -t $NUM_THREADS -B $B --max-count 3000 -d $d  -r $SUPERREADS  -p $REF_SPLIT -o $COORDS.txt.tmp && mv $COORDS.txt.tmp $COORDS.txt || error_exit "Create_mega_reads failed"
 else
@@ -208,44 +220,32 @@ fi
 
 #this is different from joining the mega-reads, because we create scaffolds, not contigs; one scaffold per reference contig
 if [ ! -s $COORDS.1.fa ] || [ -e .rerun ];then
-echo "Joining"
+log "Joining"
 awk '{if($0~/^>/){pb=substr($1,2);print $0} else { print $3" "$4" "$5" "$6" "$10" "pb" "$11" "$9}}' $COORDS.txt |join_mega_reads_trim.onepass.ref.pl 1>$COORDS.1.fa.tmp 2>/dev/null && mv $COORDS.1.fa.tmp $COORDS.1.fa
 touch .rerun
 fi
 
+if [ ! -s $COORDS.unitigs.fa ] || [ ! -s $COORDS.scaffolds.fa ] || [ -e .rerun ];then
 SR_FRG=$COORDS.sr.frg
-TCOVERAGE=20
-if [ $ESTIMATED_GENOME_SIZE -gt 1 ];then
-echo "Generating assembly input files"
+log "Running preliminary assembly"
 if [ ! -s $SR_FRG ];then
 create_sr_frg.pl 65525 < $MASURCA_ASSEMBLY_WORK1_PATH/superReadSequences.fasta | fasta2frg.pl super > $SR_FRG.tmp && mv  $SR_FRG.tmp  $SR_FRG && rm -rf $CA
 fi
-TCOVERAGE=`ls $SR_FRG $OTHER_FRG 2>/dev/null | xargs stat -c%s | awk '{n+=$1}END{print int(n/int('$ESTIMATED_GENOME_SIZE')/1.7+1)}'`;
-fi
 
-rm -f .rerun
 rm -f $CA.log
 
-OVLMIN=`head -n 100000 $SR_FRG $OTHER_FRG 2>/dev/null | grep -A 1 '^seq:' |grep -v '^seq:' | grep -v '\-\-' | awk 'BEGIN{minlen=100000}{if(length($1)<minlen && length($1)>=64) minlen=length($1);}END{if(minlen>=250) print "250"; else print minlen-5;}'`
-
-batOptions="-repeatdetect $TCOVERAGE $TCOVERAGE $TCOVERAGE -el $OVLMIN "
-
-echo "Coverage threshold for splitting unitigs is $TCOVERAGE minimum ovl $OVLMIN"
-
-echo "batOptions=$batOptions
-cnsConcurrency=$NUM_THREADS
+echo "cnsConcurrency=$NUM_THREADS
 cnsMinFrags=10000
 unitigger=bogart
 merylMemory=65536
 ovlStoreMemory=65536
 utgGraphErrorLimit=1000
 utgMergeErrorLimit=1000
-utgGraphErrorRate=0.02
-utgMergeErrorRate=0.02
+utgGraphErrorRate=0.015
+utgMergeErrorRate=0.015
 ovlCorrBatchSize=100000
-ovlCorrConcurrency=4
+ovlCorrConcurrency=8
 frgCorrThreads=$NUM_THREADS
-mbtThreads=$NUM_THREADS
 ovlThreads=2
 ovlHashBlockLength=100000000
 ovlRefBlockSize=1000000
@@ -263,27 +263,28 @@ cgwDemoteRBP=0
 cnsMaxCoverage=7
 cnsReuseUnitigs=1" > runCA.spec
 
-echo "Running assembly"
 if [ ! -e "${CA}/5-consensus/consensus.success" ]; then
-#need to start from the beginning
 rm -f $CA/0-overlaptrim-overlap/overlap.sh $CA/1-overlapper/overlap.sh $CA/5-consensus/consensus.sh
 $CA_PATH/runCA -s runCA.spec -p genome -d $CA stopAfter=consensusAfterUnitigger $SR_FRG $OTHER_FRG 1>> $CA.log 2>&1
 fi
 
-#at athis point we assume that the unitig consensus is done
 if [ ! -e "${CA}/5-consensus/consensus.success" ]; then
-echo "CA failure, see $CA.log"
-exit;
+error_exit "Preliminary assembly failure, see $CA.log"
+#else
+#$CA_PATH/tigStore -g $CA/genome.gkpStore -t $CA/genome.tigStore 2 -U -d consensus -nreads 3 100000000 > $COORDS.unitigs.fa.tmp && mv $COORDS.unitigs.fa.tmp $COORDS.unitigs.fa
 fi
 
 if [ ! -e ${CA}/recompute_astat.success ];then
-echo "Recomputing A-stat"
+log "Recomputing A-stat"
 recompute_astat_superreads_CA8.sh genome $CA $PE_AVG_READ_LENGTH $MASURCA_ASSEMBLY_WORK1_PATH/readPlacementsInSuperReads.final.read.superRead.offset.ori.txt  $SR_FRG
 touch ${CA}/recompute_astat.success
 fi
 
-#we start from here if the scaffolder has been run or continue here  
 $CA_PATH/runCA -s runCA.spec -p genome -d $CA $SR_FRG $OTHER_FRG 1>> $CA.log 2>&1
+ln -s $CA/9-terminator/genome.ctg.fasta $COORDS.unitigs.fa
+ln -s $CA/9-terminator/genome.ctg.fasta $COORDS.scaffolds.fa
+touch .rerun
+fi
 
 #now we attempt to close gaps in reference assisted scaffolds
 #if [ ! -e gapclose.success ] || [ -e .rerun ];then
@@ -307,55 +308,40 @@ $CA_PATH/runCA -s runCA.spec -p genome -d $CA $SR_FRG $OTHER_FRG 1>> $CA.log 2>&
 
 #reconcile the reference "scaffolds" with the Illumina assembly
 if [ ! -e reconcile.success ];then
-echo "Polishing reference contigs"
+log "Polishing reference contigs"
 mkdir -p reconcile
-(cd reconcile && polish_with_illumina_assembly.sh -r ../$COORDS.1.fa -q ../$CA/9-terminator/genome.ctg.fasta -t $NUM_THREADS -m 10000 1> /dev/null && perl -ane '{
-  if($F[0]=~/^>/){
-    if(length($seq)>0){
-      @f=split(/(N{1,})/,uc($seq)); 
-      my $n=1;
-      foreach $c(@f){
-        if(not($c=~/^N/) && length($c)>0){
-          $start=$n;
-          $end=$n+length($c)-1;
-          print ">$rn:$start-$end\n$c\n";
-        }
-        $n+=length($c);
-      }
-    }
-    $rn=substr($F[0],1);
-    $seq="";
-  }else{
-    $seq.=$F[0];
-  }
-}END{
-  @f=split(/(N{1,})/,uc($seq));
-  my $n=1;
-  foreach $c(@f){
-    if(not($c=~/^N/) && length($c)>0){
-      $start=$n;
-      $end=$n+length($c)-1;
-      print ">$rn:$start-$end\n$c\n";
-    }
-    $n+=length($c);
-  }
-}'  $COORDS.1.fa.genome.ctg.fasta.all.polished.deduplicated.fa > ../$COORDS.1.contigs.fa) && touch reconcile.success || error_exit "reconcile failed"
-rm -f merge.success
+(cd reconcile && polish_with_illumina_assembly.sh -r ../$COORDS.1.fa -q ../$COORDS.unitigs.fa -t $NUM_THREADS -m 10000 1> /dev/null && \
+splitScaffoldsAtNs.pl < $COORDS.1.fa.$COORDS.unitigs.fa.all.polished.deduplicated.fa > ../$COORDS.1.contigs.fa.tmp && mv ../$COORDS.1.contigs.fa.tmp ../$COORDS.1.contigs.fa ) && \
+touch reconcile.success || error_exit "reconcile failed"
+rm -f final_assembly.success
+fi
+
+#final assembly with Flye
+if [ ! -e final_assembly.success ];then
+log "Final assembly"
+cat $COORDS.1.contigs.fa $COORDS.scaffolds.fa > $COORDS.subassemblies.fa && \
+/genome2/raid/alekseyz/Flye_mod/bin/flye -t $NUM_THREADS -i 0 --subassemblies $COORDS.subassemblies.fa  --kmer-size 25 -g $ESTIMATED_GENOME_SIZE -m 250 -o flye.$COORDS 1>flye.$COORDS.log 2>&1 && \
+touch final_assembly.success || error_exit "Final assembly failure, see flye.$COORDS.log"
+fi
+
+if [ -e final_assembly.success ];then
+log "Final output sequences are in flye.$COORDS/scaffolds.fasta"
+ufasta n50 -a flye.$COORDS/scaffolds.fasta
 fi
 
 #now we merge
-if [ ! -e merge1.success ];then
-echo "Merging reference contigs 1"
-mkdir -p final_merge1 && \
-(cd final_merge1 && merge_contigs.sh -i 99 -m 1000 -r ../$COORDS.1.contigs.fa -q ../$CA/9-terminator/genome.ctg.fasta -t $NUM_THREADS 1>/dev/null && mv  $COORDS.1.contigs.fa.genome.ctg.fasta.merged.fa ../contigs.merge1.fasta) && touch merge1.success || error_exit "final merge1 failed"
-fi
+#if [ ! -e merge1.success ];then
+#echo "Merging reference contigs 1"
+#mkdir -p final_merge1 && \
+#(cd final_merge1 && merge_contigs.sh -i 99 -m 1000 -r ../$COORDS.1.contigs.fa -q ../$CA/9-terminator/genome.ctg.fasta -t $NUM_THREADS 1>/dev/null && mv  $COORDS.1.contigs.fa.genome.ctg.fasta.merged.fa ../contigs.merge1.fasta) && touch merge1.success || error_exit "final merge1 failed"
+#fi
 
-if [ ! -e merge2.success ];then
-echo "Merging reference contigs 2"
-mkdir -p final_merge2 && \
-(cd final_merge2 && merge_contigs.sh -i 98 -m 500 -r ../contigs.merge1.fasta -q ../$REF_SPLIT -t $NUM_THREADS 1>/dev/null && mv  contigs.merge1.fasta.$REF_SPLIT.merged.fa ../contigs.final.fasta) && touch merge2.success || error_exit "final merge2 failed"
-fi
+#if [ ! -e merge2.success ];then
+#echo "Merging reference contigs 2"
+#mkdir -p final_merge2 && \
+#(cd final_merge2 && merge_contigs.sh -i 98 -m 500 -r ../contigs.merge1.fasta -q ../$REF_SPLIT -t $NUM_THREADS 1>/dev/null && mv  contigs.merge1.fasta.$REF_SPLIT.merged.fa ../contigs.final.fasta) && touch merge2.success || error_exit "final merge2 failed"
+#fi
 
 
-echo "Final output contigs are in contigs.final.fasta"
-ufasta n50 -A -S -N50 -C contigs.final.fasta
+#echo "Final output contigs are in contigs.final.fasta"
+#ufasta n50 -A -S -N50 -C contigs.final.fasta
