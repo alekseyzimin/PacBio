ad1abc81ec676b0ebe3739566221641432fabdd4
Aleksey Zimin <alekseyz@ipst.umd.edu>
2019-11-04 15:28:38 -0500

    changed fill parameters to account for overhangs

--- src_reconcile/fill_unaligned_gaps.pl
+++ src_reconcile/fill_unaligned_gaps.pl
@@ -23,20 +23,30 @@ if(not($seq eq "")){
 
 my $prevref;
 my $prevend;
-my $mingap=500;
+my $mingap=1000; #minimum gap to be filled
 my $gapnum=0;
+my $gapbeg=0;
 while($line=<STDIN>){
 chomp($line);
 @f=split(/\s+/,$line);
-if($f[-2] eq $prevref && $f[0]-$prevend>$mingap){#we found a fillable gap
-my $filllen=$f[0]-$prevend-1;
-die("reference $f[-2] not found") if(not(defined($rseq{$f[-2]})));
-print STDERR ">fill$gapnum\n",substr($rseq{$f[-2]},$prevend+1,$filllen),"\n";
-print $prevend+1," ",$f[0]-1," | 1 $filllen | $filllen $filllen | 100.0 | $f[11] $filllen | .1 100.0 | $f[-2] fill$gapnum\n";
-$gapnum++;
+if($f[3]<$f[4]){
+  $gapbeg=$f[0]-$f[3]+1;
+}else{
+  $gapbeg=$f[0]-($f[12]-$f[3]);
+}
+if($f[-2] eq $prevref && $gapbeg-$prevend>$mingap){#we found a fillable gap
+  my $filllen=$gapbeg-$prevend-1;
+  die("reference $f[-2] not found") if(not(defined($rseq{$f[-2]})));
+  print STDERR ">fill$gapnum\n",substr($rseq{$f[-2]},$prevend,$filllen),"\n";
+  print $prevend+1," ",$gapbeg-1," | 1 $filllen | $filllen $filllen | 100.0 | $f[11] $filllen | .1 100.0 | $f[-2] fill$gapnum\n";
+  $gapnum++;
 }
 $prevref=$f[-2];
-$prevend=$f[1];
+if($f[3]<$f[4]){
+  $prevend=$f[1]+($f[12]-$f[4]);
+}else{
+  $prevend=$f[1]+$f[4]-1;
+}
 print $line,"\n";
 }
 
