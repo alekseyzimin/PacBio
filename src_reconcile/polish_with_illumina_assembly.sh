b9b59b46289b37029adb3e6df9c8180a59cddb3f
Aleksey Zimin <alekseyz@ipst.umd.edu>
2019-11-14 11:32:41 -0500

    added step by step reporting to poolish with illumina script

--- src_reconcile/polish_with_illumina_assembly.sh
+++ src_reconcile/polish_with_illumina_assembly.sh
@@ -11,6 +11,19 @@ function error_exit {
     echo "$1" >&2
     exit "${2:-1}"
 }
+GC=
+RC=
+NC=
+if tty -s < /dev/fd/1 2> /dev/null; then
+GC='\e[0;32m'
+RC='\e[0;31m'
+NC='\e[0m'
+fi
+
+log () {
+  dddd=$(date)
+  echo -e "${GC}[$dddd]${NC} $@"
+}
 
 
 #parsing arguments
@@ -68,17 +81,20 @@ DELTAFILE=$REFN.$QRYN
 
 #nucmer
 if [ ! -e polish_align.success ];then
+log "Initial alignment"
 nucmer -t $NUM_THREADS -p  $DELTAFILE -c 100 $REF $QRY && touch polish_align.success && rm -f filter.success || exit
 fi
 
 #delta-filter
 if [ ! -e polish_filter.success ];then
+log "Filtering alignments"
 parallel_delta-filter.sh $DELTAFILE '-1 -l 100  -o 20 ' 9 && mv $DELTAFILE.fdelta $DELTAFILE.1.delta && \
 parallel_delta-filter.sh $DELTAFILE '-q -l 100  -o 20 ' 9 && mv $DELTAFILE.fdelta $DELTAFILE.q.delta && \
 touch polish_filter.success && rm -f  polish_add_not_aligning.success || exit
 fi
 
 if [ ! -e polish_add_not_aligning.success ];then
+log "Adding missing sequences that did not align to the assembly"
 #add the sequences that did not align and longer than 1000 bp
 show-coords -lcH -I $SIMILARITY_RATE $DELTAFILE.q.delta| perl -ane '{$palign{$F[-1]}+=$F[-4];}END{foreach $k(keys %palign){print $k,"\n" if($palign{$k}>50)}}' > aligned_sequences.txt
 ufasta sizes -H $QRY | awk '{if($2<1000) print $1}' > short_sequences.txt
@@ -88,6 +104,7 @@ touch polish_add_not_aligning.success && rm -f polish_replace_consensus.success 
 fi
 
 if [ ! -e polish_replace_consensus.success ];then
+log "Polishing consensus"
 if [ $MERGE -gt 0 ];then
 show-coords -lcHr -I $SIMILARITY_RATE -L 100 $DELTAFILE.1.delta |  merge_matches_and_tile_coords_file.pl $MERGE | reconcile_consensus.pl $REFN.$QRYN.all.fa $QRY > $REFN.$QRYN.all.polished.fa && \
 touch polish_replace_consensus.success && rm -f polish_self_map.success || exit
@@ -99,6 +116,7 @@ fi
 
 #here we map the contigs against themselves to figure out which ones are redundant
 if [ ! -e polish_self_map.success ];then
+log "Self-alignment to remove duplicates"
 perl -ane 'BEGIN{$seq=""}{if($F[0] =~ /^\>/){if(length($seq)>=1000){print length($seq)," $rn $seq\n";}$seq="";$rn=$F[0];}else{$seq.=$F[0];}}END{print length($seq)," $rn $seq\n";}' $REFN.$QRYN.all.polished.fa | sort -S 50% -nrk1 | awk '{print $2"\n"$3}' >  scaffolds.ref.fa && \
 ufasta extract -f <(ufasta sizes -H $REFN.$QRYN.all.polished.fa | awk '{if($2<500000) print $1}') $REFN.$QRYN.all.polished.fa > $REFN.$QRYN.all.polished.lt500000.fa && \
 nucmer -t $NUM_THREADS --batch $(($(stat -c%s "scaffolds.ref.fa")/5)) -c 100 -b 100 -p sasm_to_sasm  scaffolds.ref.fa  $REFN.$QRYN.all.polished.lt500000.fa && \
@@ -106,13 +124,14 @@ touch polish_self_map.success && rm -f polish_filter_map.success || exit
 fi
 
 if [ ! -e polish_filter_map.success ];then
+log "Removing duplicates"
 awk 'BEGIN{p=1;}{if($1 ~/^>/){if(substr($1,2)==$2) p=0; else p=1;} if(p==1) print $0;}' sasm_to_sasm.delta > sasm_to_sasm.noself.delta && \
 delta-filter -i $SIMILARITY_RATE -q -o 20 sasm_to_sasm.noself.delta > sasm_to_sasm.noself.fdelta && \
-show-coords -lcHr sasm_to_sasm.noself.fdelta | awk '{if($12>$13) print $0}' |merge_matches_and_tile_coords_file.pl 1000 | perl -ane '{$cov{$F[-1]}+=$F[15] if($F[15]>=2);}END{foreach $k(keys %cov){print $k,"\n" if($cov{$k}>50);}}' > sduplicates.txt && \
+show-coords -lcHr sasm_to_sasm.noself.fdelta | awk '{if($12>$13) print $0}' |merge_matches_and_tile_coords_file.pl 1000 | perl -ane '{$cov{$F[-1]}+=$F[15] if($F[15]>=5);}END{foreach $k(keys %cov){print $k,"\n" if($cov{$k}>75);}}' > sduplicates.txt && \
 awk 'BEGIN{p=1;}{if($1 ~/^>/){if(substr($1,2)==$2) p=0; else p=1;} if(p==1) print $0;}' sasm_to_sasm.delta| show-coords -lcH /dev/stdin | awk '{if($12>$13 && $10>98 && $16>90) print $NF}' >> sduplicates.txt && \
 ufasta extract -v -f sduplicates.txt $REFN.$QRYN.all.polished.fa > $REFN.$QRYN.all.polished.deduplicated.fa && \
 touch polish_filter_map.success || exit
 fi
 
-echo "Output sequences in $REFN.$QRYN.all.polished.deduplicated.fa"
+log "Success!!! Output sequences in $REFN.$QRYN.all.polished.deduplicated.fa"
 ufasta n50 -A -S -N50 $REFN.$QRYN.all.polished.deduplicated.fa
