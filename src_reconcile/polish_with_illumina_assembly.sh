d36fa92da13838213bc0470a56442c631896e706
Aleksey Zimin <alekseyz@ipst.umd.edu>
2019-11-14 11:27:48 -0500

    changed the way extra sequences are added

--- src_reconcile/polish_with_illumina_assembly.sh
+++ src_reconcile/polish_with_illumina_assembly.sh
@@ -74,12 +74,13 @@ fi
 #delta-filter
 if [ ! -e polish_filter.success ];then
 parallel_delta-filter.sh $DELTAFILE '-1 -l 100  -o 20 ' 9 && mv $DELTAFILE.fdelta $DELTAFILE.1.delta && \
+parallel_delta-filter.sh $DELTAFILE '-q -l 100  -o 20 ' 9 && mv $DELTAFILE.fdelta $DELTAFILE.q.delta && \
 touch polish_filter.success && rm -f  polish_add_not_aligning.success || exit
 fi
 
 if [ ! -e polish_add_not_aligning.success ];then
 #add the sequences that did not align and longer than 1000 bp
-show-coords -lcH -I $SIMILARITY_RATE $DELTAFILE.1.delta| perl -ane '{$palign{$F[-1]}+=$F[-4];}END{foreach $k(keys %palign){print $k,"\n" if($palign{$k}>20)}}' > aligned_sequences.txt
+show-coords -lcH -I $SIMILARITY_RATE $DELTAFILE.q.delta| perl -ane '{$palign{$F[-1]}+=$F[-4];}END{foreach $k(keys %palign){print $k,"\n" if($palign{$k}>50)}}' > aligned_sequences.txt
 ufasta sizes -H $QRY | awk '{if($2<1000) print $1}' > short_sequences.txt
 ufasta extract -v -f <(cat aligned_sequences.txt short_sequences.txt) $QRY > $REFN.$QRYN.extra.fa && \
 cat $REF $REFN.$QRYN.extra.fa > $REFN.$QRYN.all.fa && \
@@ -99,15 +100,15 @@ fi
 #here we map the contigs against themselves to figure out which ones are redundant
 if [ ! -e polish_self_map.success ];then
 perl -ane 'BEGIN{$seq=""}{if($F[0] =~ /^\>/){if(length($seq)>=1000){print length($seq)," $rn $seq\n";}$seq="";$rn=$F[0];}else{$seq.=$F[0];}}END{print length($seq)," $rn $seq\n";}' $REFN.$QRYN.all.polished.fa | sort -S 50% -nrk1 | awk '{print $2"\n"$3}' >  scaffolds.ref.fa && \
-ufasta extract -f <(ufasta sizes -H $REFN.$QRYN.all.polished.fa | awk '{if($2<100000) print $1}') $REFN.$QRYN.all.polished.fa > $REFN.$QRYN.all.polished.lt100000.fa && \
-nucmer -t $NUM_THREADS --batch $(($(stat -c%s "scaffolds.ref.fa")/5)) -c 100 -b 100 -p sasm_to_sasm  scaffolds.ref.fa  $REFN.$QRYN.all.polished.lt100000.fa && \
+ufasta extract -f <(ufasta sizes -H $REFN.$QRYN.all.polished.fa | awk '{if($2<500000) print $1}') $REFN.$QRYN.all.polished.fa > $REFN.$QRYN.all.polished.lt500000.fa && \
+nucmer -t $NUM_THREADS --batch $(($(stat -c%s "scaffolds.ref.fa")/5)) -c 100 -b 100 -p sasm_to_sasm  scaffolds.ref.fa  $REFN.$QRYN.all.polished.lt500000.fa && \
 touch polish_self_map.success && rm -f polish_filter_map.success || exit
 fi
 
 if [ ! -e polish_filter_map.success ];then
 awk 'BEGIN{p=1;}{if($1 ~/^>/){if(substr($1,2)==$2) p=0; else p=1;} if(p==1) print $0;}' sasm_to_sasm.delta > sasm_to_sasm.noself.delta && \
 delta-filter -i $SIMILARITY_RATE -q -o 20 sasm_to_sasm.noself.delta > sasm_to_sasm.noself.fdelta && \
-show-coords -lcHr sasm_to_sasm.noself.fdelta | awk '{if($12>$13) print $0}' |merge_matches_and_tile_coords_file.pl 1000 | perl -ane '{$cov{$F[-1]}+=$F[15] if($F[15]>=5);}END{foreach $k(keys %cov){print $k,"\n" if($cov{$k}>60);}}' > sduplicates.txt && \
+show-coords -lcHr sasm_to_sasm.noself.fdelta | awk '{if($12>$13) print $0}' |merge_matches_and_tile_coords_file.pl 1000 | perl -ane '{$cov{$F[-1]}+=$F[15] if($F[15]>=2);}END{foreach $k(keys %cov){print $k,"\n" if($cov{$k}>50);}}' > sduplicates.txt && \
 awk 'BEGIN{p=1;}{if($1 ~/^>/){if(substr($1,2)==$2) p=0; else p=1;} if(p==1) print $0;}' sasm_to_sasm.delta| show-coords -lcH /dev/stdin | awk '{if($12>$13 && $10>98 && $16>90) print $NF}' >> sduplicates.txt && \
 ufasta extract -v -f sduplicates.txt $REFN.$QRYN.all.polished.fa > $REFN.$QRYN.all.polished.deduplicated.fa && \
 touch polish_filter_map.success || exit
