8351ca8fe94af94bd6888851f1588335375c8065
Aleksey Zimin <alekseyz@ipst.umd.edu>
2019-11-15 17:00:13 -0500

    changed default in memory usage for samtools sort

--- src_reconcile/masurca-polish.sh
+++ src_reconcile/masurca-polish.sh
@@ -5,7 +5,7 @@ MYPATH="`( cd \"$MYPATH\" && pwd )`"
 export PATH=$MYPATH:$PATH;
 set -o pipefail
 export NUM_THREADS=4
-export MEM=16000000000
+export MEM=1G
 export FIX=1
 export BATCH_SIZE=0
 GC=
@@ -62,7 +62,7 @@ do
             set -x
             ;;
         -h|--help|-u|--usage)
-            echo "Usage:  evaluate_consensus_error_rate.sh -a <assembly contigs or scaffolds> -r <'Illumina_reads_fastq1 Illumina_reads_fastq'> -t <number of threads> [-n] <optional:do not fix errors that are found>"
+            echo "Usage:  evaluate_consensus_error_rate.sh -a <assembly contigs or scaffolds> -r <'Illumina_reads_fastq1 Illumina_reads_fastq'> -t <number of threads> [-n] <optional:do not fix errors that are found> [-m] <optional: memory per thread to use in samtools sort>"
             echo "Must have bwa, samtools and freebayes available on the PATH"
             exit 0
             ;;
