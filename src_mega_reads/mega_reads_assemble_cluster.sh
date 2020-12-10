a48cc3d3b0b8733e271a926796c0b8d72df6f7e3
Aleksey Zimin <alekseyz@ipst.umd.edu>
2020-12-10 10:51:07 -0500

    changed the name for flye assembly folder

--- src_mega_reads/mega_reads_assemble_cluster.sh
+++ src_mega_reads/mega_reads_assemble_cluster.sh
@@ -790,7 +790,8 @@ fi
 if [ $FLYE -gt 0 ];then
     if [ ! -s "flye/assembly.fasta" ];then
         log "Running assembly with Flye"
-	$CA_PATH/flye -t $NUM_THREADS --nano-corr $COORDS.1$POSTFIX.fa -g $ESTIMATED_GENOME_SIZE --kmer-size 21 -m 2500 -o flye -i 0 1>flye.log 2>&1
+        echo flye.${COORDS} > FLYE_dir.txt
+	$CA_PATH/flye -t $NUM_THREADS --nano-corr $COORDS.1$POSTFIX.fa -g $ESTIMATED_GENOME_SIZE --kmer-size 21 -m 2500 -o flye.${COORDS} -i 0 1>flye.log 2>&1
     fi
 else
     if [ ! -s $COORDS.1.frg ] || [ ! -s $COORDS.1.mates.frg ] || [ -e .rerun ];then
