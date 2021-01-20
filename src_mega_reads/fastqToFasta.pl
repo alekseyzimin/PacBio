64eb9c8984ae75d7375232647827feda69507727
Aleksey Zimin <alekseyz@ipst.umd.edu>
2021-01-20 09:25:26 -0500

    made mega-reads compatible with gzipped gasta/fastq files

--- src_mega_reads/fastqToFasta.pl
+++ src_mega_reads/fastqToFasta.pl
@@ -1,9 +1,9 @@
 #!/usr/bin/env perl
 #
-#convert fastq on STDIN  to fasta on STDOUT
+#convert fastq on STDIN  to fasta on STDOUT, leave untouched if fasta
 
 while($line=<STDIN>){
-  if(substr($line,0,1) eq "@"){
+  if(substr($line,0,1) eq "@"){ #type is fastq
     print ">",substr($line,1); 
     $seq=""; 
     $nlines=0;
@@ -18,6 +18,11 @@ while($line=<STDIN>){
     while($line=<STDIN>){
       $slines++; 
       last if($slines==$nlines);
+    }
+  }elsif(substr($line,0,1) eq ">"){ #type is fasta, output unchanged
+    print $line;
+    while($line=<STDIN>){
+      print $line;
     }
   }
 }
