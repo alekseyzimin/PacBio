6d0f01657e01c9bf5e425f8239e6dfac58d08398
Aleksey Zimin <alekseyz@ipst.umd.edu>
2021-05-20 17:10:02 -0400

    main scaffolding script done

--- src_reconcile/merge_contigs.sh
+++ src_reconcile/merge_contigs.sh
@@ -4,12 +4,34 @@ MYPATH="`( cd \"$MYPATH\" && pwd )`"
 export PATH=$MYPATH:$PATH;
 set -o pipefail
 NUM_THREADS=1
-MIN_MATCH=1000
-IDENTITY=98
+MIN_MATCH=5000
+OVERHANG=1000
+GC=
+RC=
+NC=
+if tty -s < /dev/fd/1 2> /dev/null; then
+    GC='\e[0;32m'
+    RC='\e[0;31m'
+    NC='\e[0m'
+fi
+
+trap abort 1 2 15
+function abort {
+log "Aborted"
+kill 0
+exit 1
+}
+
+
+log () {
+    dddd=$(date)
+    echo -e "${GC}[$dddd]${NC} $@"
+}
 
 
 function error_exit {
-    echo "$1" >&2
+    dddd=$(date)
+    echo -e "${RC}[$dddd]${NC} $1" >&2
     exit "${2:-1}"
 }
 
@@ -24,8 +46,8 @@ do
             NUM_THREADS="$2"
             shift
             ;;
-        -i|--identity)
-            IDENTITY="$2"
+        -o|--overhang)
+            OVERHANG="$2"
             shift
             ;;
         -q|--query)
@@ -67,19 +89,34 @@ REFN=`basename $REF`
 QRYN=`basename $QRY`
 DELTAFILE=$REFN.$QRYN
 
-#nucmer
-if [ ! -e merge_align.success ];then
-nucmer -t $NUM_THREADS -p  $DELTAFILE -l 31 -c 200 $REF $QRY && touch merge_align.success && rm -f merge_filter.success || exit
+#minimap
+log "Aligning the scaffolding sequences to the contigs"
+if [ ! -e scaffold_align.success ];then
+$MYPATH/../FLye/bin/flye-minimap2 -t $NUM_THREADS -x map-ont $REF $QRY 1> $REFN.$QRYN.paf.tmp 2>minimap.err && mv $REFN.$QRYN.paf.tmp $REFN.$QRYN.paf && touch scaffold_align.success && rm -f scaffold_filter.success || error_exit "minimap2 failed"
 fi
 
-#delta-filter
-if [ ! -e merge_filter.success ];then
-parallel_delta-filter.sh $DELTAFILE "-1 -l 200" 9 && mv $DELTAFILE.fdelta $DELTAFILE.r.delta && \
-touch merge_filter.success && rm -f  merge_merge.success || exit
+#filter
+log "Filtering alignments"
+if [ ! -e scaffold_filter.success ];then
+awk '{if($4-$3>int("'$MIN_MATCH'") && ($8<int("'$OVERHANG'") || $7-$9<int("'$OVERHANG'")) && $12>=60) print $0}' $REFN.$QRYN.paf | \
+sort -k1,1 -k3,3n -S 10% | \
+awk 'BEGIN{r="";c=""}{if($1!=r){print $0" "$1;r=$1;c=$6}else if($6!=c){print $0" "$1;c=$6}}' | \
+uniq -D -f 18 | \
+awk '{if($5=="+"){print $8+1" "$9+1" | "$3+1" "$4+1" | "$11" "$4-$3" | 100 | "$7" "$2" | "int($11/$7*10000)/100" "int(($4-$3)/$2*10000)/100" | "$6" "$1}else{print $8+1" "$9+1" | "$4+1" "$3+1" | "$11" "$4-$3" | 100 | "$7" "$2" | "int($11/$7*10000)/100" "int(($4-$3)/$2*10000)/100" | "$6" "$1}}' > $REFN.$QRYN.coords.tmp && mv $REFN.$QRYN.coords.tmp $REFN.$QRYN.coords || error_exit "filtering alignments failed" 
+touch scaffold_filter.success && rm -f  scaffold_links.success 
 fi
 
-if [ ! -e merge_merge.success ];then
-show-coords -lcHq -I $IDENTITY -L $MIN_MATCH $DELTAFILE.r.delta | extract_merges.pl $QRY > merges.txt && merge_contigs.pl < merges.txt| create_merged_sequences.pl $REF merges.txt > $REFN.$QRYN.merged.fa && touch merge_merge.success || exit
+log "Creating scaffold links"
+if [ ! -e scaffold_links.success ];then
+cat  $REFN.$QRYN.coords |$MYPATH/extract_merges.pl <(ufasta extract -f <(awk '{print $NF}' $REFN.$QRYN.coords) $QRY) | \
+awk '{print $7" "$8" "$2" "$5" "$2+$5" "$1" "$3" "$4" "$6}' | sort -k6,9 -k5,5n -S 10% | uniq -f 5 | awk '{print $6" "$3" "$7" "$8" "$4" "$9" "$1" "$2}'  > $REFN.$QRYN.links.txt.tmp && mv  $REFN.$QRYN.links.txt.tmp  $REFN.$QRYN.links.txt || error_exit "creating scaffold links failed"
+touch scaffold_links.success && rm -f  scaffold_graph.success
 fi
 
-echo "Output sequences in $REFN.$QRYN.merged.fa"
+log "Creating scaffold graph and building scaffolds"
+if [ ! -e scaffold_graph.success ];then
+cat $REFN.$QRYN.links.txt | $MYPATH/merge_contigs.pl | $MYPATH/create_merged_sequences.pl $REF  $REFN.$QRYN.links.txt > $REFN.$QRYN.scaffolds.fa.tmp && mv $REFN.$QRYN.scaffolds.fa.tmp $REFN.$QRYN.scaffolds.fa || error_exit "walking the scaffold graph failed"
+fi
+
+log "Output sequences in $REFN.$QRYN.scaffolds.fa"
+ufasta n50 -a $REFN.$QRYN.scaffolds.fa
