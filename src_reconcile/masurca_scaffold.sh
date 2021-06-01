1783932915a3ace3faab9199da1e449d3784c3ad
Aleksey Zimin <alekseyz@ipst.umd.edu>
2021-06-01 11:50:54 -0400

    added removal of a conflicting link is conflict count is 1

--- src_reconcile/masurca_scaffold.sh
+++ src_reconcile/masurca_scaffold.sh
@@ -108,7 +108,9 @@ fi
 if [ ! -e scaffold_links.success ];then
 log "Creating scaffold links"
 cat  $REFN.$QRYN.coords |$MYPATH/extract_merges.pl <(ufasta extract -f <(awk '{print $NF}' $REFN.$QRYN.coords) $QRY) | \
-awk '{print $7" "$8" "$2" "$5" "$2+$5" "$1" "$3" "$4" "$6}' | sort -k6,9 -k5,5n -S 10% | uniq -f 5 | awk '{print $6" "$3" "$7" "$8" "$4" "$9" "$1" "$2}'  > $REFN.$QRYN.links.txt.tmp && mv  $REFN.$QRYN.links.txt.tmp  $REFN.$QRYN.links.txt || error_exit "creating scaffold links failed"
+awk '{print $7" "$8" "$2" "$5" "$2+$5" "$1" "$3" "$4" "$6}' | sort -k6,9 -k5,5n -S 10% | uniq -c -f 5 | awk '{print $1" "$7" "$4" "$8" "$9" "$5" "$10" "$2" "$3}'  > $REFN.$QRYN.links.counts.txt.tmp && mv  $REFN.$QRYN.links.counts.txt.tmp  $REFN.$QRYN.links.counts.txt || error_exit "creating scaffold links failed"
+#here we remove conflicting links if the conflict count is 1
+sort -nrk1,1 -S 10% $REFN.$QRYN.links.counts.txt | perl -ane 'if(not(defined($h{$F[1].$F[4]}))){$h{$F[1].$F[4]}=$F[0];print join(" ",@F[1..$#F]),"\n";}elsif($F[0]>1 || $h{$F[1].$F[4]}==1){print  join(" ",@F[1..$#F]),"\n";}' > $REFN.$QRYN.links.txt.tmp && mv $REFN.$QRYN.links.txt.tmp $REFN.$QRYN.links.txt || error_exit "creating scaffold links failed"
 touch scaffold_links.success 
 fi
 
