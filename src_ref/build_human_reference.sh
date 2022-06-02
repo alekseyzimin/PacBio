cbab5bbedf766b4e12b02d74cb58fb7719072e61
Aleksey Zimin <alekseyz@ipst.umd.edu>
2022-06-02 15:58:32 -0400

    rolled extract_chrM into the main build_human_reference script

--- src_ref/build_human_reference.sh
+++ src_ref/build_human_reference.sh
@@ -209,7 +209,11 @@ if [ ! -e  build.gapless.success ];then
   bash ./run.sh && \
   log "Building chrM" && \
   ufasta extract -f <(ufasta sizes -H chromosomes.fa | grep chrM |awk '{print $1}') chromosomes.fa > chrM.draft.fa && \
-  $MYPATH/extract_chrM.sh $CHM13PATH/chrM.fa chrM.draft.fa && \
+  nucmer -p chrM -c 200 -t 32 $CHM13PATH/chrM.fa chrM.draft.fa && \
+  ASM_ctg=`delta-filter -r -o 0 chrM.delta |show-coords -lcHr /dev/stdin | perl -ane '{$ctg=$F[-1];if($F[3]<$F[4]){$c1=$F[3];$c2=$F[4];}else{$c1=$F[4];$c2=$F[3];}END{print $ctg,"\n"}}'` && \
+  ASM_beg=`delta-filter -r -o 0 chrM.delta |show-coords -lcHr /dev/stdin | perl -ane '{$ctg=$F[-1];if($F[3]<$F[4]){$c1=$F[3];$c2=$F[4];}else{$c1=$F[4];$c2=$F[3];}END{print $c1,"\n"}}'` && \
+  ASM_end=`delta-filter -r -o 0 chrM.delta |show-coords -lcHr /dev/stdin | perl -ane '{$ctg=$F[-1];if($F[3]<$F[4]){$c1=$F[3];$c2=$F[4];}else{$c1=$F[4];$c2=$F[3];}END{print $c2,"\n"}}'` && \
+  ufasta extract -n $ASM_ctg chrM.draft.fa |ufasta one |grep -v '^>' |awk '{print ">chrM\n"substr($1,'$ASM_beg',('$ASM_end'-'$ASM_beg'+1))}' > chrM.new.fa && \
   if [ $GENDER = "male" ];then
     log "Building Y" && \
     mkdir -p Y.dir && \
